40c612062a70a3afa9d6deb2abcdcb6751bf0988
Umair Ali <umairmamoor@gmail.com>
2025-04-13 22:15:20 -0400

    time complexity

--- TimeAndSpaceComplexity/src/dev/lpa/Main.java
+++ TimeAndSpaceComplexity/src/dev/lpa/Main.java
@@ -3,6 +3,16 @@ package dev.lpa;
 /*
 why time complexity matters - it tells how fast or slow an algorithm runs on the size of input
 - it also measures efficiency.
+
+O(1), O(n), O(n2), O(logn), O(nlogn), O(2n), O(n2)
+
+O stands for big-O notation -> language of time complexity, n is the size of the input.
+
+we focus on worst case scenarios, ignore constants, focus on only high order items.
+
+O(n) -> its like a airport security. The more people there are, the more time it will take
+O(n2)-> comparing everyone to everyone.
+
  */
 
 public class Main {
