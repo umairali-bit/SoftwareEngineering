ef07bf4aa9c09edca227f198da5bae7ff0b1fdf0
Umair Ali <umairmamoor@gmail.com>
2025-05-29 04:24:16 -0400

    creating an object in the springboot application

---
+++ Backend/MODULE1/1.2/IntroductionToSpringBoot/src/main/java/com/devShuttle/umair/Week1Introduction/IntroductionToSpringBoot/Apple.java
@@ -0,0 +1,9 @@
+package com.devShuttle.umair.Week1Introduction.IntroductionToSpringBoot;
+
+public class Apple {
+
+
+    void eatApple() {
+        System.out.println("I am eating the apple");
+    }
+}
