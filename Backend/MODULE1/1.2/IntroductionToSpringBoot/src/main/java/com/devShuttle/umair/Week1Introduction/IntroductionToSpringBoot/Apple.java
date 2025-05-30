803a1fbd2786ef37313f5c04792a49e2491a9c8c
Umair Ali <umairmamoor@gmail.com>
2025-05-30 03:19:51 -0400

    @PostConstruct bean

--- Backend/MODULE1/1.2/IntroductionToSpringBoot/src/main/java/com/devShuttle/umair/Week1Introduction/IntroductionToSpringBoot/Apple.java
+++ Backend/MODULE1/1.2/IntroductionToSpringBoot/src/main/java/com/devShuttle/umair/Week1Introduction/IntroductionToSpringBoot/Apple.java
@@ -1,5 +1,6 @@
 package com.devShuttle.umair.Week1Introduction.IntroductionToSpringBoot;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.stereotype.Component;
 
 //@Component //@Component in Spring is a stereotype annotation that marks a class as a Spring-managed bean
@@ -8,5 +9,10 @@ public class Apple {
 
     void eatApple() {
         System.out.println("I am eating the apple");
+    }
+
+    @PostConstruct
+    void callThisBeforeAppleIsUsed() {
+        System.out.println("Creating the apple before its use");
     }
 }
