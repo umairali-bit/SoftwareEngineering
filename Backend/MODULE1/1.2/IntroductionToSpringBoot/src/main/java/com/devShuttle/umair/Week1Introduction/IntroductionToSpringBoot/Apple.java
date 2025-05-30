44c4f3632105be0c3a5eb6cc60d344b8a5a6d62b
Umair Ali <umairmamoor@gmail.com>
2025-05-30 03:24:03 -0400

    @PreDestroy bean

--- Backend/MODULE1/1.2/IntroductionToSpringBoot/src/main/java/com/devShuttle/umair/Week1Introduction/IntroductionToSpringBoot/Apple.java
+++ Backend/MODULE1/1.2/IntroductionToSpringBoot/src/main/java/com/devShuttle/umair/Week1Introduction/IntroductionToSpringBoot/Apple.java
@@ -1,6 +1,7 @@
 package com.devShuttle.umair.Week1Introduction.IntroductionToSpringBoot;
 
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.springframework.stereotype.Component;
 
 //@Component //@Component in Spring is a stereotype annotation that marks a class as a Spring-managed bean
@@ -14,5 +15,11 @@ public class Apple {
     @PostConstruct
     void callThisBeforeAppleIsUsed() {
         System.out.println("Creating the apple before its use");
+    }
+
+    @PreDestroy //works after the bean is destroyed, after springboot stop running
+    void callThisBeforeDestroy() {
+        System.out.println("Destroying the apple bean");
+
     }
 }
