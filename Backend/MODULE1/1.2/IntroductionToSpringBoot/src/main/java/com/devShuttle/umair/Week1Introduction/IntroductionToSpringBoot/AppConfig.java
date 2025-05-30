179c1806d8dcd0dc6bb0ea33ec730a39b213bac8
Umair Ali <umairmamoor@gmail.com>
2025-05-30 02:49:58 -0400

    the @Configuration annotation

---
+++ Backend/MODULE1/1.2/IntroductionToSpringBoot/src/main/java/com/devShuttle/umair/Week1Introduction/IntroductionToSpringBoot/AppConfig.java
@@ -0,0 +1,12 @@
+package com.devShuttle.umair.Week1Introduction.IntroductionToSpringBoot;
+
+
+import org.springframework.context.annotation.Configuration;
+
+/*
+In Spring Boot (and Spring in general), a configuration class is a special class annotated with
+@Configuration that is used to define beans manually and provide configuration settings to the Spring container.
+ */
+@Configuration
+public class AppConfig {
+}
