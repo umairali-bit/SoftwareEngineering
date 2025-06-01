d8d03a72c22861c6c40237d2255571f5994cc9b3
Umair Ali <umairmamoor@gmail.com>
2025-06-01 10:08:01 -0400

    @ConditionalOnProperty

--- Backend/MODULE1/1.4/DependencyInjection/src/main/java/DependencyInjection/DevDB.java
+++ Backend/MODULE1/1.4/DependencyInjection/src/main/java/DependencyInjection/DevDB.java
@@ -1,8 +1,10 @@
 package DependencyInjection;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
 
 @Component
+@ConditionalOnProperty(name = "deploy.env", havingValue = "development")
 public class DevDB implements DB{
 
    public String getData() {
--- Backend/MODULE1/1.4/DependencyInjection/src/main/java/DependencyInjection/ProdDB.java
+++ Backend/MODULE1/1.4/DependencyInjection/src/main/java/DependencyInjection/ProdDB.java
@@ -1,15 +1,13 @@
 package DependencyInjection;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 @Component
-@Primary
+@ConditionalOnProperty(name = "deploy.env", havingValue = "production")
 public class ProdDB implements DB {
-
-    @Autowired
-    private DevDB db;
 
     public  String getData() {
         return "Prod Data";
