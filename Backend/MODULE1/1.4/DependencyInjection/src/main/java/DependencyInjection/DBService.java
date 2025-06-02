8bb2544b573c1c8918d41b0d7cad7f6073b86548
Umair Ali <umairmamoor@gmail.com>
2025-06-02 02:55:03 -0400

    use of constructor injection

--- Backend/MODULE1/1.4/DependencyInjection/src/main/java/DependencyInjection/DBService.java
+++ Backend/MODULE1/1.4/DependencyInjection/src/main/java/DependencyInjection/DBService.java
@@ -6,8 +6,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class DBService {
 
-    @Autowired
-    private DB db;
+
+    final private DB db;
+
+    public DBService(DB db) {
+
+        this.db = db;
+
+    }
 
     String getData() {
         return db.getData();
