2ccd4cc0e2cfc63c497d6372911df76b7dda6899
Umair Ali <umairmamoor@gmail.com>
2025-06-05 17:16:21 -0400

    testing file

---
+++ Backend/MODULE1/1.5/SpringBootvsSpringFramework/src/test/java/com/example/springbootvsframework/Maintest.java
@@ -0,0 +1,13 @@
+package com.example.springbootvsframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MainTest {
+
+    @Test
+    void contextLoads() {
+
+    }
+}
