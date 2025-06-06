299d0b89cc77b16dbf24a2aa8dc87d4251fbce4b
Umair Ali <umairmamoor@gmail.com>
2025-06-06 05:24:55 -0400

    sometimes torubleshooting could take hours

---
+++ Backend/MODULE1/moduleRecap/src/test/java/com/example/moduleRecap/ModuleRecapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moduleRecap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModuleRecapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
