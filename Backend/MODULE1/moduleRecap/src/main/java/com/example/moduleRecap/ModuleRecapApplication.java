11bef39af0173425b2318f42cfa0fa72df3aa343
Umair Ali <umairmamoor@gmail.com>
2025-06-06 05:24:55 -0400

    sometimes torubleshooting could take hours

---
+++ Backend/MODULE1/moduleRecap/src/main/java/com/example/moduleRecap/ModuleRecapApplication.java
@@ -0,0 +1,13 @@
+package com.example.moduleRecap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModuleRecapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModuleRecapApplication.class, args);
+	}
+
+}
