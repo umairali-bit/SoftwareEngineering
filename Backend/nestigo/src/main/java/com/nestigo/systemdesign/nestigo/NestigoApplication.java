ae9a47bf35a44ab0bd7d3147d2c3ace88962f6c3
Umair Ali <umairmamoor@gmail.com>
2025-06-08 11:06:44 -0400

    nestigo

---
+++ Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/NestigoApplication.java
@@ -0,0 +1,13 @@
+package com.nestigo.systemdesign.nestigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestigoApplication.class, args);
+	}
+
+}
