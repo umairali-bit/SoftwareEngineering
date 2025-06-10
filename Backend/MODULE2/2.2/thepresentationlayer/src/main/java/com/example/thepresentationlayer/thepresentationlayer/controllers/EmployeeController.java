c39746d44cd0d2a8ee2e23a7e30e3042a20b22ea
Umair Ali <umairmamoor@gmail.com>
2025-06-10 03:44:20 -0400

    Jackson converted the data into JSON, @GetMapping url

--- Backend/MODULE2/2.2/thepresentationlayer/src/main/java/com/example/thepresentationlayer/thepresentationlayer/controllers/EmployeeController.java
+++ Backend/MODULE2/2.2/thepresentationlayer/src/main/java/com/example/thepresentationlayer/thepresentationlayer/controllers/EmployeeController.java
@@ -1,8 +1,12 @@
 package com.example.thepresentationlayer.thepresentationlayer.controllers;
 
 
+import com.example.thepresentationlayer.thepresentationlayer.dto.EmployeeDTO;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
 
 @RestController
 public class EmployeeController {
@@ -12,6 +16,12 @@ public class EmployeeController {
 //        return "Secret Message: asdfal@#$DASD";
 //    }
 
+@GetMapping("/employees/{employeeId}")
+    public EmployeeDTO getEmployeeById(@PathVariable Long employeeId) {
+        return new EmployeeDTO(employeeId, "Umair Ali", "umairmamoor@gmail.com", 27,
+                    LocalDate.of(2024, 1, 2), true);
+
+}
 
 
 }
