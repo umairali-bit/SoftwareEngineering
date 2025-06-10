a5da96007aeb16b895933a60ff3a7f359feacf99
Umair Ali <umairmamoor@gmail.com>
2025-06-10 02:59:04 -0400

    The presentation Layer, DTO, and Controllers

---
+++ Backend/MODULE2/2.2/thepresentationlayer/src/main/java/com/example/thepresentationlayer/thepresentationlayer/ThepresentationlayerApplication.java
@@ -0,0 +1,13 @@
+package com.example.thepresentationlayer.thepresentationlayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThepresentationlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThepresentationlayerApplication.class, args);
+	}
+
+}
---
+++ Backend/MODULE2/2.2/thepresentationlayer/src/test/java/com/example/thepresentationlayer/thepresentationlayer/ThepresentationlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.thepresentationlayer.thepresentationlayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThepresentationlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
