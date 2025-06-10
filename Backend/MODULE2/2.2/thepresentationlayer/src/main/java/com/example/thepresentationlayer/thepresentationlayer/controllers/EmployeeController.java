4a460982d9cb7cd77a3a4b4f5ca9b460b1762ed0
Umair Ali <umairmamoor@gmail.com>
2025-06-10 03:54:46 -0400

    @RequestParam(required=false)

--- Backend/MODULE2/2.2/thepresentationlayer/src/main/java/com/example/thepresentationlayer/thepresentationlayer/controllers/EmployeeController.java
+++ Backend/MODULE2/2.2/thepresentationlayer/src/main/java/com/example/thepresentationlayer/thepresentationlayer/controllers/EmployeeController.java
@@ -4,9 +4,11 @@ package com.example.thepresentationlayer.thepresentationlayer.controllers;
 import com.example.thepresentationlayer.thepresentationlayer.dto.EmployeeDTO;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @RestController
 public class EmployeeController {
@@ -16,12 +18,22 @@ public class EmployeeController {
 //        return "Secret Message: asdfal@#$DASD";
 //    }
 
-@GetMapping("/employees/{employeeId}")
+    @GetMapping(path = "/employees/{employeeId}")
     public EmployeeDTO getEmployeeById(@PathVariable Long employeeId) {
         return new EmployeeDTO(employeeId, "Umair Ali", "umairmamoor@gmail.com", 27,
-                    LocalDate.of(2024, 1, 2), true);
+                LocalDate.of(2024, 1, 2), true);
 
+    }
+
+    @GetMapping(path = "/employees")
+        public String getAllEmployees(@RequestParam(required = false) Integer age) {
+
+            return "Hi age" + age;
+
+
+        }
 }
 
 
-}
+
+
