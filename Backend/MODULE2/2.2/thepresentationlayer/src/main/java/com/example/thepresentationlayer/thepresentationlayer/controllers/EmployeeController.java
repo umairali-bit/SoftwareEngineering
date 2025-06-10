a95cc4c55091478a56d8a11c0fe69f8bdd2b116b
Umair Ali <umairmamoor@gmail.com>
2025-06-10 03:16:18 -0400

    Annotations and URL

--- Backend/MODULE2/2.2/thepresentationlayer/src/main/java/com/example/thepresentationlayer/thepresentationlayer/controllers/EmployeeController.java
+++ Backend/MODULE2/2.2/thepresentationlayer/src/main/java/com/example/thepresentationlayer/thepresentationlayer/controllers/EmployeeController.java
@@ -1,4 +1,15 @@
 package com.example.thepresentationlayer.thepresentationlayer.controllers;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmployeeController {
+
+    @GetMapping(path = "/getSecretMessage()")
+    public String getMySuperSecretMessage() {
+        return "Secret Message: asdfal@#$DASD";
+    }
+
 }
