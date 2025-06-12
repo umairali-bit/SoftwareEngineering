c6b425128053ea06d1078cb04b360becb3b23672
Umair Ali <umairmamoor@gmail.com>
2025-06-12 06:48:41 -0400

    The persistence Layer and JPA Repository

---
+++ Backend/MODULE2/2.3/ThePersistenceLayer/src/main/java/com/jpa/thepersistencelayerandjpa/ThePersistenceLayer/ThePersistenceLayerApplication.java
@@ -0,0 +1,13 @@
+package com.jpa.thepersistencelayerandjpa.ThePersistenceLayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThePersistenceLayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThePersistenceLayerApplication.class, args);
+	}
+
+}
