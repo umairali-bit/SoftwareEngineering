25056ba63f759ae73e851fe74d90ea90035fc493
Umair Ali <umairmamoor@gmail.com>
2025-06-13 04:30:49 -0400

    adding the H2 URL

--- Backend/MODULE2/2.3/ThePersistenceLayer/src/main/java/com/jpa/thepersistencelayerandjpa/ThePersistenceLayer/repositories/EmployeeRepository.java
+++ Backend/MODULE2/2.3/ThePersistenceLayer/src/main/java/com/jpa/thepersistencelayerandjpa/ThePersistenceLayer/repositories/EmployeeRepository.java
@@ -2,8 +2,13 @@ package com.jpa.thepersistencelayerandjpa.ThePersistenceLayer.repositories;
 
 import com.jpa.thepersistencelayerandjpa.ThePersistenceLayer.entities.EmployeeEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
+@Repository
 public interface EmployeeRepository extends JpaRepository<EmployeeEntity, Long> {
+    
 
 
 }
