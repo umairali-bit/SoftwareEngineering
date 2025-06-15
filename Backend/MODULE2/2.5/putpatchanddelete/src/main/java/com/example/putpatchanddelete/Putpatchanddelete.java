949050620a4238c566d0dc7ad1d177756a1dad37
Umair Ali <umairmamoor@gmail.com>
2025-06-15 17:20:48 -0400

    @Putmapping

---
+++ Backend/MODULE2/2.5/putpatchanddelete/src/main/java/com/example/putpatchanddelete/Putpatchanddelete.java
@@ -0,0 +1,13 @@
+package com.example.putpatchanddelete;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Putpatchanddelete {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Putpatchanddelete.class, args);
+	}
+
+}
