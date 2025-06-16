28f1e0cf1d1324f742fae2b1276171b765702d82
Umair Ali <umairmamoor@gmail.com>
2025-06-16 04:47:28 -0400

    Input Validation Annotations, Creating Custom Annptations for Validations

---
+++ Backend/MODULE2/2.6/inputvalidation/src/main/java/com/example/inputvalidation/inputvalidation/InputvalidationApplication.java
@@ -0,0 +1,13 @@
+package com.example.inputvalidation.inputvalidation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InputvalidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InputvalidationApplication.class, args);
+	}
+
+}
---
+++ Backend/MODULE2/2.6/inputvalidation/src/main/java/com/example/inputvalidation/inputvalidation/entities/EmployeeEntity.java
@@ -0,0 +1,91 @@
+package com.example.inputvalidation.inputvalidation.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "employees")
+public class EmployeeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String email;
+    private Integer age;
+    private LocalDate dateOfJoining;
+    private Boolean isActive;
+
+//    public EmployeeEntity() {}
+//
+//    public Long getId() {
+//        return id;
+//    }
+//
+//    public void setId(Long id) {
+//        this.id = id;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public Integer getAge() {
+//        return age;
+//    }
+//
+//    public void setAge(Integer age) {
+//        this.age = age;
+//    }
+//
+//    public LocalDate getDateOfJoining() {
+//        return dateOfJoining;
+//    }
+//
+//    public void setDateOfJoining(LocalDate dateOfJoining) {
+//        this.dateOfJoining = dateOfJoining;
+//    }
+//
+//    public Boolean getActive() {
+//        return isActive;
+//    }
+//
+//    public void setActive(Boolean active) {
+//        isActive = active;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return "EmployeeEntity{" +
+//                "id=" + id +
+//                ", name='" + name + '\'' +
+//                ", email='" + email + '\'' +
+//                ", age=" + age +
+//                ", dateOfJoining=" + dateOfJoining +
+//                ", isActive=" + isActive +
+//                '}';
+//    }
+//}
+}
