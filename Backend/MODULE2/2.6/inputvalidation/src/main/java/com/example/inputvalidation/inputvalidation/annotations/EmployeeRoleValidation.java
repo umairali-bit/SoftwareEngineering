e8d2999b3553e0f8ec3fc85892a335f88da11051
Umair Ali <umairmamoor@gmail.com>
2025-06-16 07:14:58 -0400

    Custom annotations

---
+++ Backend/MODULE2/2.6/inputvalidation/src/main/java/com/example/inputvalidation/inputvalidation/annotations/EmployeeRoleValidation.java
@@ -0,0 +1,24 @@
+package com.example.inputvalidation.inputvalidation.annotations;
+
+import jakarta.validation.Payload;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.PARAMETER})
+public @interface EmployeeRoleValidation {
+
+    String message() default "Role of an Employee can either be User and ADMIN";
+
+    Class<?>[] groups() default {};
+
+    Class<? extends Payload>[] payload() default {};
+
+
+
+
+
+}
