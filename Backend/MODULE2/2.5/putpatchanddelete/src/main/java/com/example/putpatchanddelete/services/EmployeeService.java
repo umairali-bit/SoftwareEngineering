03a8acf9e461e08e36e27564eca7f0aee72c2bfd
Umair Ali <umairmamoor@gmail.com>
2025-06-16 03:08:02 -0400

    Patch mapping using Reflection API

--- Backend/MODULE2/2.5/putpatchanddelete/src/main/java/com/example/putpatchanddelete/services/EmployeeService.java
+++ Backend/MODULE2/2.5/putpatchanddelete/src/main/java/com/example/putpatchanddelete/services/EmployeeService.java
@@ -4,8 +4,11 @@ import com.example.putpatchanddelete.entities.EmployeeEntity;
 import com.example.putpatchanddelete.repositories.EmployeeRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ReflectionUtils;
 
+import java.lang.reflect.Field;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -59,12 +62,39 @@ public class EmployeeService {
 
     }
 
+    public boolean existsByEmployeeId(Long employeeId) {
+       return employeeRepository.existsById(employeeId);
+    }
+
     public boolean deleteEmployeeById(Long employeeId) {
 
-        boolean exist = employeeRepository.existsById(employeeId);
+        boolean exist = existsByEmployeeId(employeeId);
         if(!exist) return false;
         employeeRepository.deleteById(employeeId);
         return true;
+
+
+    }
+
+    public EmployeeDTO updatePartialEmployeeById(Long employeeId, Map<String, Object> updates) {
+        boolean exist = existsByEmployeeId(employeeId);
+        if(!exist) return null;
+        EmployeeEntity employeeEntity = employeeRepository.findById(employeeId).orElseThrow(() -> new RuntimeException(
+                "Employee not found"));
+
+        updates.forEach((field, value) -> {
+            Field fieldToUpdate = ReflectionUtils.findField(EmployeeEntity.class, field);
+            fieldToUpdate.setAccessible(true);
+            ReflectionUtils.setField(fieldToUpdate, employeeEntity, value);
+        });
+
+        return modelMapper.map(employeeRepository.save(employeeEntity), EmployeeDTO.class);
+
+
+
+
+
+
 
 
     }
