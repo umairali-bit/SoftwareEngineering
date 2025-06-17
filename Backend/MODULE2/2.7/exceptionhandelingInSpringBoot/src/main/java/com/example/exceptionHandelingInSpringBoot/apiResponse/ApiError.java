3ccb3060bfd1f7cd7f7e7d08a80bb6ad735d3dd1
Umair Ali <umairmamoor@gmail.com>
2025-06-17 04:05:35 -0400

    ApiError class with ApiResponse fields

---
+++ Backend/MODULE2/2.7/exceptionhandelingInSpringBoot/src/main/java/com/example/exceptionHandelingInSpringBoot/apiResponse/ApiError.java
@@ -0,0 +1,20 @@
+package com.example.exceptionHandelingInSpringBoot.apiResponse;
+
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
+
+@Data
+@Builder
+public class ApiError {
+
+    private HttpStatus status;
+    private String message;
+    private List<String> subErrors;
+
+
+
+}
