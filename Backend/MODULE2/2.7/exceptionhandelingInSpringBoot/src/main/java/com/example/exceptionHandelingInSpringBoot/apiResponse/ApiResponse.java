0cc428c0fe70f8966b167aa5a9a00089f4e0b99b
Umair Ali <umairmamoor@gmail.com>
2025-06-17 04:05:35 -0400

    ApiError class with ApiResponse fields

--- Backend/MODULE2/2.7/exceptionhandelingInSpringBoot/src/main/java/com/example/exceptionHandelingInSpringBoot/apiResponse/ApiResponse.java
+++ Backend/MODULE2/2.7/exceptionhandelingInSpringBoot/src/main/java/com/example/exceptionHandelingInSpringBoot/apiResponse/ApiResponse.java
@@ -1,4 +1,19 @@
 package com.example.exceptionHandelingInSpringBoot.apiResponse;
 
-public class ApiResponse {
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class ApiResponse<T> {
+
+    private LocalDateTime timeStamp;
+    private T data;
+    private ApiError error;
+
+    public ApiResponse() {
+        this.timeStamp = LocalDateTime.now();
+    }
+
+    public ApiResponse(T data) {
+        this.data = data;
+    }
 }
