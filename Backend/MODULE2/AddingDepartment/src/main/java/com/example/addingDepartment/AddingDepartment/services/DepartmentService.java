6ef7f40a11684a3040275ec51813b2213dfa56ec
Umair Ali <umairmamoor@gmail.com>
2025-06-18 02:40:24 -0400

    adding getDepartmentById service

--- Backend/MODULE2/AddingDepartment/src/main/java/com/example/addingDepartment/AddingDepartment/services/DepartmentService.java
+++ Backend/MODULE2/AddingDepartment/src/main/java/com/example/addingDepartment/AddingDepartment/services/DepartmentService.java
@@ -1,21 +1,32 @@
 package com.example.addingDepartment.AddingDepartment.services;
 
+import com.example.addingDepartment.AddingDepartment.dto.DepartmentDTO;
 import com.example.addingDepartment.AddingDepartment.repositories.DepartmentRepository;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
 @Service
 public class DepartmentService {
 
-    private final DepartmentRepository repository;
+    private final DepartmentRepository departmentRepository;
     private final ModelMapper modelMapper;
 
 
-    public DepartmentService(DepartmentRepository repository, ModelMapper modelMapper) {
-        this.repository = repository;
+    public DepartmentService(DepartmentRepository departmentRepository, ModelMapper modelMapper) {
+        this.departmentRepository = departmentRepository;
         this.modelMapper = modelMapper;
     }
+
+
+    public Optional<DepartmentDTO> getDepartmentById(Long departmentId){
+        return  departmentRepository.findById(departmentId)
+                .map(departmentEntity -> modelMapper.map(departmentEntity, DepartmentDTO.class));
+
+    }
+
 
 
 }
