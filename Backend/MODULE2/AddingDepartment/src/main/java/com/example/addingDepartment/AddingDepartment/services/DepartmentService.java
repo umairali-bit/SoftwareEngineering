10f765f10a5462ab54d85c39849f955ab3e8506e
Umair Ali <umairmamoor@gmail.com>
2025-06-18 02:44:11 -0400

    getting all departments in service

--- Backend/MODULE2/AddingDepartment/src/main/java/com/example/addingDepartment/AddingDepartment/services/DepartmentService.java
+++ Backend/MODULE2/AddingDepartment/src/main/java/com/example/addingDepartment/AddingDepartment/services/DepartmentService.java
@@ -1,12 +1,15 @@
 package com.example.addingDepartment.AddingDepartment.services;
 
 import com.example.addingDepartment.AddingDepartment.dto.DepartmentDTO;
+import com.example.addingDepartment.AddingDepartment.entities.DepartmentEntity;
 import com.example.addingDepartment.AddingDepartment.repositories.DepartmentRepository;
 
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class DepartmentService {
@@ -26,6 +29,16 @@ public class DepartmentService {
                 .map(departmentEntity -> modelMapper.map(departmentEntity, DepartmentDTO.class));
 
     }
+
+    public List<DepartmentDTO> getAllDepartments() {
+        List<DepartmentEntity> departmentEntities = departmentRepository.findAll();
+        return departmentEntities
+                .stream()
+                .map(departmentEntity -> modelMapper.map(departmentEntity, DepartmentDTO.class))
+                .collect(Collectors.toList());
+    }
+
+
 
 
 
