3f808360fac99dca93d1ee92f68d4807d68ea3fc
Umair Ali <umairmamoor@gmail.com>
2025-06-18 03:44:57 -0400

    Post controller

--- Backend/MODULE2/AddingDepartment/src/main/java/com/example/addingDepartment/AddingDepartment/controllers/DepartmentController.java
+++ Backend/MODULE2/AddingDepartment/src/main/java/com/example/addingDepartment/AddingDepartment/controllers/DepartmentController.java
@@ -4,11 +4,9 @@ package com.example.addingDepartment.AddingDepartment.controllers;
 import com.example.addingDepartment.AddingDepartment.dto.DepartmentDTO;
 import com.example.addingDepartment.AddingDepartment.exceptions.ResourceNotFound;
 import com.example.addingDepartment.AddingDepartment.services.DepartmentService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -35,6 +33,14 @@ public class DepartmentController {
                 .map(departmentDTO1 -> ResponseEntity.ok(departmentDTO1))
                 .orElseThrow(() -> new ResourceNotFound("Department NOT found with id: " + departmentId));
     }
+
+    @PostMapping
+    public ResponseEntity<DepartmentDTO> createDepartments(@RequestBody DepartmentDTO departmentDTO) {
+        DepartmentDTO savedDepartment = departmentService.createDepartment(departmentDTO);
+        return new ResponseEntity<>(savedDepartment, HttpStatus.CREATED);
+    }
+
+
 
 
 }
