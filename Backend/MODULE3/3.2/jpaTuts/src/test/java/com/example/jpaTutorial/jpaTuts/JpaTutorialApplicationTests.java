3444c56a884cc2e79c8f9e8c37cc7ae2885e53dc
Umair Ali <umairmamoor@gmail.com>
2025-06-19 14:04:41 -0400

    Hibernate ORM, JPA, Entities, and Tables in Spring Data JPA

---
+++ Backend/MODULE3/3.2/jpaTuts/src/test/java/com/example/jpaTutorial/jpaTuts/JpaTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaTutorial.jpaTuts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
