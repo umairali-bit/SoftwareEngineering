5b5b4dc6fbddf8b7ef4070dc19978b1c6a310751
Umair Ali <umairmamoor@gmail.com>
2025-06-19 14:32:13 -0400

    adding properties

--- Backend/MODULE3/3.2/jpaTuts/src/main/java/com/example/jpaTutorial/jpaTuts/entities/Product.java
+++ Backend/MODULE3/3.2/jpaTuts/src/main/java/com/example/jpaTutorial/jpaTuts/entities/Product.java
@@ -1,6 +1,41 @@
 package com.example.jpaTutorial.jpaTuts.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String sku;
+
+    private String tile;
+
+    private BigDecimal price;
+
+    private Integer quantity;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
 
 
 }
