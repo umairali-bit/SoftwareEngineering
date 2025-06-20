257d45816b5b82133d484a0c767286e04c3fd4df
Umair Ali <umairmamoor@gmail.com>
2025-06-20 02:45:54 -0400

    adding necessary files

---
+++ Backend/MODULE3/3.3/jpaInter/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO test_db.product_table (price, quantity, sku, title_x)
+Values
+(12.4, 4, 'gingerale734', 'Ginerale'),
+(14.4, 2, 'pepsi120', 'Pepsi'),
+(16.2, 1, 'pepsi156', 'Soft Drink'),
+(16.2, 10, 'Coke1234', 'Soft Drink1');
