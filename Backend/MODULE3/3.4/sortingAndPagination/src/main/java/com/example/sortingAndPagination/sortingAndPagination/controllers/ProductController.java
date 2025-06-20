4f4a35d53c4a1d70f2059072c1ac1ba462695d1a
Umair Ali <umairmamoor@gmail.com>
2025-06-20 06:35:48 -0400

    accessing repository from controller not recommended

--- Backend/MODULE3/3.4/sortingAndPagination/src/main/java/com/example/sortingAndPagination/sortingAndPagination/controllers/ProductController.java
+++ Backend/MODULE3/3.4/sortingAndPagination/src/main/java/com/example/sortingAndPagination/sortingAndPagination/controllers/ProductController.java
@@ -1,4 +1,19 @@
 package com.example.sortingAndPagination.sortingAndPagination.controllers;
 
+import com.example.sortingAndPagination.sortingAndPagination.repositories.ProductRepository;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/products")
 public class ProductController {
+
+    private final ProductRepository productRepository;
+
+
+    public ProductController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+
 }
