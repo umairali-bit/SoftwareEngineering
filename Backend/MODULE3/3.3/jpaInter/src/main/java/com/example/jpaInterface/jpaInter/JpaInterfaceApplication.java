60f964ec1018df168a02d53d1c7a65270900ba11
Umair Ali <umairmamoor@gmail.com>
2025-06-20 02:43:50 -0400

    Spring Data JPA Interfaces and Dynamic Query Methods

---
+++ Backend/MODULE3/3.3/jpaInter/src/main/java/com/example/jpaInterface/jpaInter/JpaInterfaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.jpaInterface.jpaInter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaInterfaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaInterfaceApplication.class, args);
+	}
+
+}
