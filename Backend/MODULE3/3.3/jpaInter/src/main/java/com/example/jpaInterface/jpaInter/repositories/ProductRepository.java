25624651cab3b95eb1e06ad53069bbdf8f0a42de
Umair Ali <umairmamoor@gmail.com>
2025-06-20 04:10:45 -0400

    using AND in custom queries

--- Backend/MODULE3/3.3/jpaInter/src/main/java/com/example/jpaInterface/jpaInter/repositories/ProductRepository.java
+++ Backend/MODULE3/3.3/jpaInter/src/main/java/com/example/jpaInterface/jpaInter/repositories/ProductRepository.java
@@ -4,6 +4,7 @@ import com.example.jpaInterface.jpaInter.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
@@ -13,4 +14,5 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
     List<ProductEntity> findByTitle(String soft_drink);
     List<ProductEntity> findByCreatedAtAfter(LocalDateTime after);
+    List<ProductEntity> findByQuantityAndPrice(int quantity, BigDecimal price);
 }
