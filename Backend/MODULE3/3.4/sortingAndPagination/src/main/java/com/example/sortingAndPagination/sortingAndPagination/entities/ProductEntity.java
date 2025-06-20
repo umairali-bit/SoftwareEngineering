19e7c36b5b46dd87f5d8e27253ef6f740329a069
Umair Ali <umairmamoor@gmail.com>
2025-06-20 06:21:05 -0400

    Pagination and sorting

---
+++ Backend/MODULE3/3.4/sortingAndPagination/src/main/java/com/example/sortingAndPagination/sortingAndPagination/entities/ProductEntity.java
@@ -0,0 +1,52 @@
+package com.example.sortingAndPagination.sortingAndPagination.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(
+        name = "product_table",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "tile_price_unique", columnNames = {"title_x","price"})
+        },
+        indexes = {
+                @Index(name = "sku_index", columnList = "sku")
+        }
+)
+public class ProductEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 20)
+    private String sku;
+
+    @Column(name = "title_x")
+    private String title;
+
+    private BigDecimal price;
+
+    private Integer quantity;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    private LocalDateTime updatedAt;
+
+
+
+}
---
+++ Backend/MODULE3/3.4/sortingAndPagination/src/main/java/com/example/sortingAndPagination/sortingAndPagination/repositories/ProductRepository.java
@@ -0,0 +1,37 @@
+package com.example.sortingAndPagination.sortingAndPagination.repositories;
+
+
+import com.example.sortingAndPagination.sortingAndPagination.entities.ProductEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
+
+
+    List<ProductEntity> findByTitle(String soft_drink);
+    List<ProductEntity> findByCreatedAtAfter(LocalDateTime after);
+    List<ProductEntity> findByQuantityAndPrice(int quantity, BigDecimal price);
+
+    List<ProductEntity> findByQuantityGreaterThanAndPriceLessThan(int quantity, BigDecimal price);
+    List<ProductEntity> findByQuantityGreaterThanOrPriceLessThan(int quantity, BigDecimal price);
+    List<ProductEntity> findByTitleLike(String title);
+
+    List<ProductEntity> findByTitleContaining(String title);
+    List<ProductEntity> findByTitleContainingIgnoreCase(String title);
+
+    //   Optional<ProductEntity> findByTitleAndPrice(String title, BigDecimal price);
+
+    @Query("select e from ProductEntity e where e.title = :title and e.price = :price")
+    Optional<ProductEntity> findByTitleAndPrice(@Param("title") String title, @Param("price") BigDecimal price);
+
+
+
+}
