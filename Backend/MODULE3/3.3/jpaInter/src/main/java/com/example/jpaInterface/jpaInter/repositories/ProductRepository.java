3cd3b0796c2ee8f64007230553cf6fe621c806e5
Umair Ali <umairmamoor@gmail.com>
2025-06-20 04:55:29 -0400

    JPQL queries

--- Backend/MODULE3/3.3/jpaInter/src/main/java/com/example/jpaInterface/jpaInter/repositories/ProductRepository.java
+++ Backend/MODULE3/3.3/jpaInter/src/main/java/com/example/jpaInterface/jpaInter/repositories/ProductRepository.java
@@ -2,6 +2,8 @@ package com.example.jpaInterface.jpaInter.repositories;
 
 import com.example.jpaInterface.jpaInter.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.math.BigDecimal;
@@ -24,5 +26,11 @@ public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
     List<ProductEntity> findByTitleContaining(String title);
     List<ProductEntity> findByTitleContainingIgnoreCase(String title);
 
-    Optional<ProductEntity> findByTitleAndPrice(String title, BigDecimal price);
+ //   Optional<ProductEntity> findByTitleAndPrice(String title, BigDecimal price);
+
+    @Query("select e from ProductEntity e where e.title = :title and e.price = :price")
+    Optional<ProductEntity> findByTitleAndPrice(@Param("title") String title, @Param("price") BigDecimal price);
+
+
+
 }
