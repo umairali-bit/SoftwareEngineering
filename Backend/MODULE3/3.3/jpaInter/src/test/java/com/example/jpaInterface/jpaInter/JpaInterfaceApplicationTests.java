8ed9807d8308bd475a87b062c74b4fbbbe75e9f7
Umair Ali <umairmamoor@gmail.com>
2025-06-20 02:43:50 -0400

    Spring Data JPA Interfaces and Dynamic Query Methods

---
+++ Backend/MODULE3/3.3/jpaInter/src/test/java/com/example/jpaInterface/jpaInter/JpaInterfaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.jpaInterface.jpaInter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaInterfaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
