6da7c16131d57a596a97840fe23ea95b90b15695
Umair Ali <umairmamoor@gmail.com>
2025-06-20 03:17:48 -0400

    testing testRepository

--- Backend/MODULE3/3.3/jpaInter/src/main/java/com/example/jpaInterface/jpaInter/repositories/ProductRepository.java
+++ Backend/MODULE3/3.3/jpaInter/src/main/java/com/example/jpaInterface/jpaInter/repositories/ProductRepository.java
@@ -2,7 +2,9 @@ package com.example.jpaInterface.jpaInter.repositories;
 
 import com.example.jpaInterface.jpaInter.entities.ProductEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ProductRepository extends JpaRepository<ProductEntity, Long> {
 
 
