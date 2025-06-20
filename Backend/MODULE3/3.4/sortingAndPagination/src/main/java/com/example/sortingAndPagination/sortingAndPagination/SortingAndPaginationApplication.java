69a772edbb47e78aa80d484030c5bb44f5ce956c
Umair Ali <umairmamoor@gmail.com>
2025-06-20 06:21:05 -0400

    Pagination and sorting

---
+++ Backend/MODULE3/3.4/sortingAndPagination/src/main/java/com/example/sortingAndPagination/sortingAndPagination/SortingAndPaginationApplication.java
@@ -0,0 +1,13 @@
+package com.example.sortingAndPagination.sortingAndPagination;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SortingAndPaginationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SortingAndPaginationApplication.class, args);
+	}
+
+}
---
+++ Backend/MODULE3/3.4/sortingAndPagination/src/test/java/com/example/sortingAndPagination/sortingAndPagination/SortingAndPaginationApplicationTests.java
@@ -0,0 +1,85 @@
+package com.example.sortingAndPagination.sortingAndPagination;
+
+
+import com.example.sortingAndPagination.sortingAndPagination.entities.ProductEntity;
+import com.example.sortingAndPagination.sortingAndPagination.repositories.ProductRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Data
+@SpringBootTest
+class JpaInterfaceApplicationTests {
+
+	@Autowired
+	ProductRepository productRepository;
+
+	@Test
+	void contextLoads() {
+	}
+
+	@Test
+	void testRepository() {
+		ProductEntity productEntity = ProductEntity.builder()
+				.sku("nestle145")
+				.title("Nestle Chocolate Milk")
+				.price(BigDecimal.valueOf(45.23))
+				.quantity(15)
+				.build();
+
+		ProductEntity savedEntity = productRepository.save(productEntity);
+		System.out.println(savedEntity);
+	}
+
+	@Test
+	void getRepository() {
+		List<ProductEntity> entities = productRepository.findAll();
+		System.out.println(entities);
+	}
+
+	@Test
+	void getTitle() {
+		List<ProductEntity> entities = productRepository.findByTitle("Soft Drink");
+		List<ProductEntity> entities1 = productRepository.findByCreatedAtAfter(
+				LocalDateTime.of(2024,1, 1,0,0,0 ));
+
+		List<ProductEntity> entities2 = productRepository.findByQuantityAndPrice(
+				4,BigDecimal.valueOf(23.45));
+		List<ProductEntity> entities3 = productRepository.findByQuantityGreaterThanAndPriceLessThan(
+				4,BigDecimal.valueOf(23.45));
+		List<ProductEntity> entities4 = productRepository.findByQuantityGreaterThanOrPriceLessThan(
+				4,BigDecimal.valueOf(23.45));
+		List<ProductEntity> entities5 = productRepository.findByTitleLike("%S%");
+		List<ProductEntity> entities6 = productRepository.findByTitleContaining("P");
+		List<ProductEntity> entities7 = productRepository.findByTitleContainingIgnoreCase("p");
+
+
+		System.out.println(entities);
+		System.out.println(entities1);
+		System.out.println(entities2);
+		System.out.println(entities3);
+		System.out.println(entities4);
+		System.out.println(entities5);
+		System.out.println(entities6);
+		System.out.println(entities7);
+
+	}
+
+	@Test
+	void getSingle() {
+		Optional <ProductEntity> productEntity = productRepository.findByTitleAndPrice
+				("Pepsi", BigDecimal.valueOf(14.4));
+
+		productEntity.ifPresentOrElse(
+				p -> System.out.println("Found: " + p),
+				() -> System.out.println("Product not found.")
+		);
+	}
+
+}
