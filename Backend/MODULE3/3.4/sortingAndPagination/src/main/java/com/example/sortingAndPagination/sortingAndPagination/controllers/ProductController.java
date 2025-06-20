b95f5990b411d0fd68521650702bc91aacdd512c
Umair Ali <umairmamoor@gmail.com>
2025-06-20 06:52:06 -0400

    testing sorting

--- Backend/MODULE3/3.4/sortingAndPagination/src/main/java/com/example/sortingAndPagination/sortingAndPagination/controllers/ProductController.java
+++ Backend/MODULE3/3.4/sortingAndPagination/src/main/java/com/example/sortingAndPagination/sortingAndPagination/controllers/ProductController.java
@@ -1,8 +1,12 @@
 package com.example.sortingAndPagination.sortingAndPagination.controllers;
 
+import com.example.sortingAndPagination.sortingAndPagination.entities.ProductEntity;
 import com.example.sortingAndPagination.sortingAndPagination.repositories.ProductRepository;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "/products")
@@ -13,6 +17,11 @@ public class ProductController {
 
     public ProductController(ProductRepository productRepository) {
         this.productRepository = productRepository;
+    }
+
+    @GetMapping
+    public List<ProductEntity> getAllProducts() {
+        return productRepository.findByTitleOrderByPrice("coca cola");
     }
 
 
