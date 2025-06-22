a18a59f314318ca3ef0a1ad1286e9e97def9ef27
Umair Ali <umairmamoor@gmail.com>
2025-06-22 18:38:17 -0400

    project with mapping

---
+++ Backend/MODULE3/3.5/Mappings/src/main/java/com/example/Mappings/Mappings/controllers/EmployeeController.java
@@ -0,0 +1,26 @@
+package com.example.Mappings.Mappings.controllers;
+
+import com.example.Mappings.Mappings.entities.EmployeeEntity;
+import com.example.Mappings.Mappings.services.EmployeeService;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(path = "/employees")
+public class EmployeeController {
+
+    private final EmployeeService employeeService;
+
+    public EmployeeController(EmployeeService employeeService) {
+        this.employeeService = employeeService;
+    }
+
+    @GetMapping("/{employeeId}")
+    public EmployeeEntity getEmployeeById(@PathVariable Long employeeId) {
+        return employeeService.getEmployeeById(employeeId);
+    }
+
+    @PostMapping
+    public EmployeeEntity createNewEmployee(@RequestBody EmployeeEntity employeeEntity) {
+        return employeeService.createNewEmployee(employeeEntity);
+    }
+}
---
+++ Backend/MODULE3/3.5/Mappings/src/main/java/com/example/Mappings/Mappings/services/EmployeeService.java
@@ -0,0 +1,24 @@
+package com.example.Mappings.Mappings.services;
+
+
+import com.example.Mappings.Mappings.entities.EmployeeEntity;
+import com.example.Mappings.Mappings.repositories.EmployeeRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmployeeService {
+
+    private final EmployeeRepository employeeRepository;
+    public EmployeeService(EmployeeRepository employeeRepository) {
+        this.employeeRepository = employeeRepository;
+    }
+
+    public EmployeeEntity createNewEmployee(EmployeeEntity employeeEntity) {
+        return employeeRepository.save(employeeEntity);
+    }
+
+    public EmployeeEntity getEmployeeById(Long id) {
+        return employeeRepository.findById(id).orElse(null);
+    }
+
+}
