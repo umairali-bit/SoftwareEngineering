889cc8dfd2ed95b97338e8c8ca2550f4d70f9bcc
Umair Ali <umairmamoor@gmail.com>
2025-06-22 08:50:40 -0400

    pagination

--- Backend/MODULE3/3.4/sortingAndPagination/src/main/java/com/example/sortingAndPagination/sortingAndPagination/controllers/ProductController.java
+++ Backend/MODULE3/3.4/sortingAndPagination/src/main/java/com/example/sortingAndPagination/sortingAndPagination/controllers/ProductController.java
@@ -2,6 +2,9 @@ package com.example.sortingAndPagination.sortingAndPagination.controllers;
 
 import com.example.sortingAndPagination.sortingAndPagination.entities.ProductEntity;
 import com.example.sortingAndPagination.sortingAndPagination.repositories.ProductRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,6 +18,8 @@ import java.util.Optional;
 @RequestMapping(path = "/products")
 public class ProductController {
 
+    private final int PAGE_SIZE = 5;
+
     private final ProductRepository productRepository;
 
 
@@ -22,10 +27,10 @@ public class ProductController {
         this.productRepository = productRepository;
     }
 
-    @GetMapping
-    public List<ProductEntity> getAllProductsByTitle() {
-        return productRepository.findByTitleOrderByPrice("coca cola");
-    }
+//    @GetMapping
+//    public List<ProductEntity> getAllProductsByTitle() {
+//        return productRepository.findByTitleOrderByPrice("coca cola");
+//    }
 
     @GetMapping(path = "/")
     public List<ProductEntity> getAllProducts() {
@@ -48,8 +53,14 @@ public class ProductController {
     }
 
     @GetMapping(path = "/pagination")
-    public List<ProductEntity> getProducts() {
-        return productRepository.findAll();
+    public List<ProductEntity> getProducts(@RequestParam(defaultValue = "id") String sortBy,
+                                           @RequestParam(defaultValue = "0") Integer pageNumber) {
+
+        Pageable pageable = PageRequest.of(
+                pageNumber,
+                PAGE_SIZE,
+                Sort.by(sortBy, "price"));
+        return productRepository.findAll(pageable).getContent();
     }
 
 
