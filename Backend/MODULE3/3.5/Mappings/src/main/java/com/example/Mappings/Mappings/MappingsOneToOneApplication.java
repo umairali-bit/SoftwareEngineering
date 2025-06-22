45280e1597b19b2d02dfa005c5ca84d26a9ddee0
Umair Ali <umairmamoor@gmail.com>
2025-06-22 11:18:08 -0400

    Spring Data JPA MAppings Part-1 - One to One Mapping

---
+++ Backend/MODULE3/3.5/Mappings/src/main/java/com/example/Mappings/Mappings/MappingsOneToOneApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mappings.Mappings;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MappingsOneToOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MappingsOneToOneApplication.class, args);
+	}
+
+}
---
+++ Backend/MODULE3/3.5/Mappings/src/test/java/com/example/Mappings/Mappings/MappingsOneToOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Mappings.Mappings;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MappingsOneToOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
