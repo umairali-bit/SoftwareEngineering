6abbc08dacda2b49aceb4e1809c3d57dc3c347da
Umair Ali <umairmamoor@gmail.com>
2025-06-23 04:26:01 -0400

    assignManager controller and Service

--- Backend/MODULE3/3.5/Mappings/src/main/java/com/example/Mappings/Mappings/services/DepartmentService.java
+++ Backend/MODULE3/3.5/Mappings/src/main/java/com/example/Mappings/Mappings/services/DepartmentService.java
@@ -2,20 +2,25 @@ package com.example.Mappings.Mappings.services;
 
 
 import com.example.Mappings.Mappings.entities.DepartmentEntity;
+import com.example.Mappings.Mappings.entities.EmployeeEntity;
 import com.example.Mappings.Mappings.repositories.DepartmentRepository;
 import com.example.Mappings.Mappings.repositories.EmployeeRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class DepartmentService {
 
     private final DepartmentRepository departmentRepository;
+    private final EmployeeRepository employeeRepository;
 
 
 
-    public DepartmentService(DepartmentRepository departmentRepository) {
+    public DepartmentService(DepartmentRepository departmentRepository, EmployeeRepository employeeRepository) {
         this.departmentRepository = departmentRepository;
 
+        this.employeeRepository = employeeRepository;
     }
 
     public DepartmentEntity createNewDepartment (DepartmentEntity departmentEntity) {
@@ -26,6 +31,20 @@ public class DepartmentService {
 
     public DepartmentEntity getDepartmentById (Long DepartId) {
         return departmentRepository.findById(DepartId).orElse(null);
+    }
+
+    public DepartmentEntity assignManager(Long departmentId, Long employeeId) {
+        Optional<DepartmentEntity> departmentEntity = departmentRepository.findById(departmentId);
+        Optional<EmployeeEntity> employeeEntity = employeeRepository.findById(employeeId);
+
+        return departmentEntity.flatMap(department ->
+                employeeEntity.map(employee -> {
+                    department.setManager(employee);
+                    return departmentRepository.save(department);
+                })
+        ).orElse(null);
+
+
     }
 }
 
