1da9a061bba3bc4a0054122e61aad09d59b31199
Umair Ali <umairmamoor@gmail.com>
2025-06-23 04:00:15 -0400

    intiating a foreing key

--- Backend/MODULE3/3.5/Mappings/src/main/java/com/example/Mappings/Mappings/entities/DepartmentEntity.java
+++ Backend/MODULE3/3.5/Mappings/src/main/java/com/example/Mappings/Mappings/entities/DepartmentEntity.java
@@ -17,11 +17,19 @@ import java.util.Set;
 @Table(name = "departments")
 public class DepartmentEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
     private String title;
+
+    @OneToOne
+    @JoinColumn(name = "department_manager")
+    private EmployeeEntity manager;
+
+
+
+
 
 
 }
