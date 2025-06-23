2cb3cde4308e54fbc3594cebfe819745b9123c2c
Umair Ali <umairmamoor@gmail.com>
2025-06-23 03:24:27 -0400

    Mappings basic project

--- Backend/MODULE3/3.5/Mappings/src/main/java/com/example/Mappings/Mappings/entities/DepartmentEntity.java
+++ Backend/MODULE3/3.5/Mappings/src/main/java/com/example/Mappings/Mappings/entities/DepartmentEntity.java
@@ -17,31 +17,11 @@ import java.util.Set;
 @Table(name = "departments")
 public class DepartmentEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
     @Column(nullable = false)
     private String title;
 
-    @OneToOne
-    @JoinColumn(name = "department_manager")
-    private EmployeeEntity manager;
 
-    @OneToMany(mappedBy = "workerDepartment", fetch = FetchType.LAZY)
-    private Set<EmployeeEntity> workers;
-
-    @ManyToMany(mappedBy = "freelanceDepartments")
-    private Set<EmployeeEntity> freelancers;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof DepartmentEntity that)) return false;
-        return Objects.equals(getId(), that.getId()) && Objects.equals(getTitle(), that.getTitle());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getTitle());
-    }
 }
--- Backend/MODULE3/3.5/Mappings/src/main/java/com/example/Mappings/Mappings/entities/EmployeeEntity.java
+++ Backend/MODULE3/3.5/Mappings/src/main/java/com/example/Mappings/Mappings/entities/EmployeeEntity.java
@@ -25,35 +25,6 @@ public class EmployeeEntity {
     @Column(nullable = false)
     private String name;
 
-    @OneToOne(mappedBy = "manager")
-    @JsonIgnore
-    private DepartmentEntity managedDepartment;
-
-    @ManyToOne(cascade = CascadeType.ALL)
-//    @JoinColumn(name = "worker_department_id")
-    @JoinTable(name = "worker_department_mapping")
-    @JsonIgnore
-    private DepartmentEntity workerDepartment;
-
-    @ManyToMany
-    @JoinTable(name = "freelancer_department_mapping",
-            joinColumns = @JoinColumn(name = "employee_id"),
-            inverseJoinColumns = @JoinColumn(name = "department_id")
-    )
-    @JsonIgnore
-    private Set<DepartmentEntity> freelanceDepartments;
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof EmployeeEntity that)) return false;
-        return Objects.equals(getId(), that.getId()) && Objects.equals(getName(), that.getName());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getName());
-    }
 
 
 }
