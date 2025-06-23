535012768493e507370c356ac3919d3416ec2dc8
Umair Ali <umairmamoor@gmail.com>
2025-06-23 16:27:36 -0400

    College Managment System

---
+++ Backend/collegeManagementSystem/src/test/java/com/collegeManagementSystem/collegeManagementSystem/CollegeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collegeManagementSystem.collegeManagementSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollegeManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
