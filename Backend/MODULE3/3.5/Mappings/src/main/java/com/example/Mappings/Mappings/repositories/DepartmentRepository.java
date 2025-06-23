7927dd88c1361a6813adf742ec00e5babc332932
Umair Ali <umairmamoor@gmail.com>
2025-06-23 05:28:02 -0400

    getting manager from department entity

--- Backend/MODULE3/3.5/Mappings/src/main/java/com/example/Mappings/Mappings/repositories/DepartmentRepository.java
+++ Backend/MODULE3/3.5/Mappings/src/main/java/com/example/Mappings/Mappings/repositories/DepartmentRepository.java
@@ -8,4 +8,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface DepartmentRepository extends JpaRepository<DepartmentEntity, Long> {
 
+    DepartmentEntity findByManager(EmployeeEntity employeeEntity);
 }
