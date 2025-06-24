fddf97a3b1baff6dc427846fc1ec425f19cf77e9
Umair Ali <umairmamoor@gmail.com>
2025-06-24 03:17:21 -0400

    adding lombok

--- Backend/collegeManagementSystem/src/main/java/com/collegeManagementSystem/collegeManagementSystem/entities/StudentEntity.java
+++ Backend/collegeManagementSystem/src/main/java/com/collegeManagementSystem/collegeManagementSystem/entities/StudentEntity.java
@@ -2,11 +2,19 @@ package com.collegeManagementSystem.collegeManagementSystem.entities;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
 
 @Entity
 @Table(name = "students")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class StudentEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
