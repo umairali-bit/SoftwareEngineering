28526dc4ff302421da60bb01f914b6b01310c081
Umair Ali <umairmamoor@gmail.com>
2025-06-24 04:13:20 -0400

    service interface and its implementations

---
+++ Backend/collegeManagementSystem/src/main/java/com/collegeManagementSystem/collegeManagementSystem/services/ProfessorServiceImpl.java
@@ -0,0 +1,66 @@
+package com.collegeManagementSystem.collegeManagementSystem.services;
+
+import com.collegeManagementSystem.collegeManagementSystem.dto.ProfessorDTO;
+import com.collegeManagementSystem.collegeManagementSystem.entities.ProfessorEntity;
+import com.collegeManagementSystem.collegeManagementSystem.repositories.ProfessorRepository;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class ProfessorServiceImpl implements ProfessorService{
+
+    private final ProfessorRepository professorRepository;
+    private final ModelMapper modelMapper;
+
+    public ProfessorServiceImpl(ProfessorRepository professorRepository, ModelMapper modelMapper) {
+        this.professorRepository = professorRepository;
+        this.modelMapper = modelMapper;
+    }
+
+
+    @Override
+    public List<ProfessorDTO> getAllProfessors() {
+        List<ProfessorEntity> professors = professorRepository.findAll();
+        return professors.stream()
+                .map(professor -> modelMapper.map(professor, ProfessorDTO.class))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public Optional<ProfessorDTO> getProfessorById(Long id) {
+        return professorRepository.findById(id)
+                .map(professorEntity -> modelMapper.map(professorEntity, ProfessorDTO.class));
+    }
+
+
+    @Override
+    public ProfessorDTO createProfessor(ProfessorDTO inputProfessor) {
+
+        ProfessorEntity professorEntity = modelMapper.map(inputProfessor, ProfessorEntity.class);
+        ProfessorEntity saved = professorRepository.save(professorEntity);
+        return modelMapper.map(saved, ProfessorDTO.class);
+
+    }
+
+    @Override
+    public ProfessorDTO updateProfessor(Long id, ProfessorDTO professorDTO) {
+        return professorRepository.findById(id).map(professor -> {
+            professor.setTitle(professorDTO.getTitle());
+
+            ProfessorEntity updated = professorRepository.save(professor);
+            return modelMapper.map(updated, ProfessorDTO.class);
+
+        }).orElseThrow(() -> new RuntimeException());
+    }
+
+    @Override
+    public boolean deleteProfessor(Long id) {
+        professorRepository.deleteById(id);
+        return true;
+
+    }
+}
