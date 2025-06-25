a9601d0228b91ccf17658edba16b74b0d94ec07d
Umair Ali <umairmamoor@gmail.com>
2025-06-25 02:42:50 -0400

    Admission Record Service

--- Backend/collegeManagementSystem/src/main/java/com/collegeManagementSystem/collegeManagementSystem/dto/AdmissionRecordDTO.java
+++ Backend/collegeManagementSystem/src/main/java/com/collegeManagementSystem/collegeManagementSystem/dto/AdmissionRecordDTO.java
@@ -1,8 +1,13 @@
 package com.collegeManagementSystem.collegeManagementSystem.dto;
 
+import lombok.Data;
+
+@Data
 public class AdmissionRecordDTO {
 
     private Long id;
     private Integer fees;
     private StudentDTO student;
+
+
 }
---
+++ Backend/collegeManagementSystem/src/main/java/com/collegeManagementSystem/collegeManagementSystem/services/AdmissionRecordImpl.java
@@ -0,0 +1,72 @@
+package com.collegeManagementSystem.collegeManagementSystem.services;
+
+import com.collegeManagementSystem.collegeManagementSystem.dto.AdmissionRecordDTO;
+import com.collegeManagementSystem.collegeManagementSystem.entities.AdmissionRecordEntity;
+import com.collegeManagementSystem.collegeManagementSystem.repositories.AdmissionRecordRepository;
+import org.modelmapper.ModelMapper;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.stream.Collectors;
+
+public class AdmissionRecordImpl implements AdmissionRecordService{
+
+    private final AdmissionRecordRepository admissionRecordRepository;
+    private final ModelMapper modelMapper;
+
+    public AdmissionRecordImpl(AdmissionRecordRepository admissionRecordRepository, ModelMapper modelMapper) {
+        this.admissionRecordRepository = admissionRecordRepository;
+        this.modelMapper = modelMapper;
+    }
+
+
+    @Override
+    public List<AdmissionRecordDTO> getAllAdmissions() {
+       List<AdmissionRecordEntity> admissions = admissionRecordRepository.findAll();
+       return admissions.stream()
+               .map(admission -> modelMapper.map(admission, AdmissionRecordDTO.class))
+               .collect(Collectors.toList());
+    }
+
+    @Override
+    public AdmissionRecordDTO getAdmissionById(Long id) {
+        AdmissionRecordEntity admission = admissionRecordRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Admission record not found with id: " + id));
+        return modelMapper.map(admission,AdmissionRecordDTO.class);
+
+    }
+
+    @Override
+    public AdmissionRecordDTO createAdmissionRecord(AdmissionRecordDTO admissionRecordDTO) {
+        AdmissionRecordEntity entity = modelMapper.map(admissionRecordDTO, AdmissionRecordEntity.class);
+        AdmissionRecordEntity saved = admissionRecordRepository.save(entity);
+        return modelMapper.map(saved,AdmissionRecordDTO.class);
+    }
+
+    @Override
+    public AdmissionRecordDTO updateAdmission(Long id, AdmissionRecordDTO admissionRecordDTO) {
+        AdmissionRecordEntity record = admissionRecordRepository.findById(id)
+                .orElseThrow(() -> new NoSuchElementException("Admission record not found with id: " + + id));
+        record.setFees(admissionRecordDTO.getFees());
+
+        AdmissionRecordEntity updated = admissionRecordRepository.save(record);
+
+        return modelMapper.map(updated, AdmissionRecordDTO.class);
+    }
+
+    @Override
+    public AdmissionRecordDTO getAdmissionRecordByStudentId(Long studentId) {
+        AdmissionRecordEntity record = admissionRecordRepository.findByStudent_id(studentId)
+                .orElseThrow(() -> new NoSuchElementException("No admission record found for student ID: " + studentId));
+        return modelMapper.map(record, AdmissionRecordDTO.class);
+    }
+
+    @Override
+    public boolean deleteAdmissionRecord (Long id){
+        if (!admissionRecordRepository.existsById(id)) {
+            throw new NoSuchElementException("Admission record not found with id: " + id);
+        }
+        admissionRecordRepository.deleteById(id);
+        return true;
+    }
+}
