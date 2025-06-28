3c2aaf2db4ba6595442080988c01e7aebae4cf0b
Umair Ali <umairmamoor@gmail.com>
2025-06-28 19:39:52 -0400

    @Getting mappings

--- Backend/libraryManagementSystem/src/main/java/com/libraryManagementSystem/libraryManagementSystem/controllers/AuthorController.java
+++ Backend/libraryManagementSystem/src/main/java/com/libraryManagementSystem/libraryManagementSystem/controllers/AuthorController.java
@@ -1,4 +1,38 @@
 package com.libraryManagementSystem.libraryManagementSystem.controllers;
 
+
+import com.libraryManagementSystem.libraryManagementSystem.dto.AuthorDTO;
+import com.libraryManagementSystem.libraryManagementSystem.services.AuthorService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/authors")
 public class AuthorController {
+
+    private final AuthorService authorService;
+
+
+    public AuthorController(AuthorService authorService) {
+        this.authorService = authorService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<AuthorDTO>> getAllAuthors() {
+        return ResponseEntity.ok(authorService.getAllAuthors());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<AuthorDTO> getAuthorById (@PathVariable  Long id) {
+        return authorService.getAuthorById(id)
+                .map(author -> ResponseEntity.ok(author))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+
 }
