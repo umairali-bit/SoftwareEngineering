0bf962f9de0233d0f9565555cfce26a0ede5a7ec
Umair Ali <umairmamoor@gmail.com>
2025-06-28 17:41:15 -0400

    getAllAuthors()

--- Backend/libraryManagementSystem/src/main/java/com/libraryManagementSystem/libraryManagementSystem/services/AuthorServiceImpl.java
+++ Backend/libraryManagementSystem/src/main/java/com/libraryManagementSystem/libraryManagementSystem/services/AuthorServiceImpl.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.NoSuchElementException;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class AuthorServiceImpl implements  AuthorService{
@@ -50,7 +51,10 @@ public class AuthorServiceImpl implements  AuthorService{
 
     @Override
     public List<AuthorDTO> getAllAuthors() {
-        return null;
+        List<AuthorEntity> authors = authorRepository.findAll();
+        return authors.stream()
+                .map(author -> convertToAuthorDTO(author))
+                .collect(Collectors.toList());
     }
 
     @Override
