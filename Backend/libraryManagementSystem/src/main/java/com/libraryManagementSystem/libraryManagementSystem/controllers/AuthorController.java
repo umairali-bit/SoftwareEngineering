0f1678247a4b8f66ae069f885d5e58a1d7e3253d
Umair Ali <umairmamoor@gmail.com>
2025-06-28 19:43:11 -0400

    @PostMapping

--- Backend/libraryManagementSystem/src/main/java/com/libraryManagementSystem/libraryManagementSystem/controllers/AuthorController.java
+++ Backend/libraryManagementSystem/src/main/java/com/libraryManagementSystem/libraryManagementSystem/controllers/AuthorController.java
@@ -3,11 +3,9 @@ package com.libraryManagementSystem.libraryManagementSystem.controllers;
 
 import com.libraryManagementSystem.libraryManagementSystem.dto.AuthorDTO;
 import com.libraryManagementSystem.libraryManagementSystem.services.AuthorService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -32,6 +30,12 @@ public class AuthorController {
         return authorService.getAuthorById(id)
                 .map(author -> ResponseEntity.ok(author))
                 .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<AuthorDTO> createAuthor (@RequestBody AuthorDTO authorDTO) {
+        AuthorDTO created = authorService.createAuthor(authorDTO);
+        return new ResponseEntity<>(created, HttpStatus.CREATED);
     }
 
 
