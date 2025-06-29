67cd2cf56061324710c9afaa53d1fbde8b83bf44
Umair Ali <umairmamoor@gmail.com>
2025-06-29 05:09:32 -0400

    @GET by id

--- Backend/libraryManagementSystem/src/main/java/com/libraryManagementSystem/libraryManagementSystem/controllers/BookController.java
+++ Backend/libraryManagementSystem/src/main/java/com/libraryManagementSystem/libraryManagementSystem/controllers/BookController.java
@@ -5,6 +5,7 @@ import com.libraryManagementSystem.libraryManagementSystem.dto.BookDTO;
 import com.libraryManagementSystem.libraryManagementSystem.services.BookService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +26,12 @@ public class BookController {
     @GetMapping
     public ResponseEntity<List<BookDTO>> getAllBooks() {
         return ResponseEntity.ok(bookService.getAllBooks());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<BookDTO> getBookById(@PathVariable Long id) {
+        return bookService.getBookById(id)
+                .map(book -> ResponseEntity.ok(book))
+                .orElse(ResponseEntity.notFound().build());
     }
 }
