d8c63b6f830da816834d03f397d8f9274cd46cc6
Umair Ali <umairmamoor@gmail.com>
2025-06-29 05:05:14 -0400

    @Get all book

--- Backend/libraryManagementSystem/src/main/java/com/libraryManagementSystem/libraryManagementSystem/controllers/BookController.java
+++ Backend/libraryManagementSystem/src/main/java/com/libraryManagementSystem/libraryManagementSystem/controllers/BookController.java
@@ -1,4 +1,29 @@
 package com.libraryManagementSystem.libraryManagementSystem.controllers;
 
+
+import com.libraryManagementSystem.libraryManagementSystem.dto.BookDTO;
+import com.libraryManagementSystem.libraryManagementSystem.services.BookService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
 public class BookController {
+
+    private final BookService bookService;
+
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<BookDTO>> getAllBooks() {
+        return ResponseEntity.ok(bookService.getAllBooks());
+    }
 }
