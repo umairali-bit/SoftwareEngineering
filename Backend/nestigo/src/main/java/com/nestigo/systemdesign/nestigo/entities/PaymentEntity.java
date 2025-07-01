730b823a068804271d3ed7cc78bf62c5205b0b49
Umair Ali <umairmamoor@gmail.com>
2025-07-01 06:20:54 -0400

    paymentEntity

--- Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/entities/PaymentEntity.java
+++ Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/entities/PaymentEntity.java
@@ -1,9 +1,12 @@
 package com.nestigo.systemdesign.nestigo.entities;
 
 
+import com.nestigo.systemdesign.nestigo.entities.enums.PaymentStatus;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.math.BigDecimal;
 
 @Entity
 @Getter
@@ -17,5 +20,12 @@ public class PaymentEntity {
     @Column(unique = false, nullable = false)
     private String transactionID;
 
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private PaymentStatus paymentStatus;
+
+    @Column(nullable = false, precision = 10, scale = 2)
+    private BigDecimal amount;
 
 }
--- Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/entities/enums/PaymentStatus.java
+++ Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/entities/enums/PaymentStatus.java
@@ -1,4 +1,7 @@
 package com.nestigo.systemdesign.nestigo.entities.enums;
 
 public enum PaymentStatus {
+
+    PENDING,
+    CANCELLED
 }
