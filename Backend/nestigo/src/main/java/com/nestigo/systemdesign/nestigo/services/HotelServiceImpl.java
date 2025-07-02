f49fdd98fbced2e18d01ca0a3bacd9130fba73dc
Umair Ali <umairmamoor@gmail.com>
2025-07-02 02:35:09 -0400

    createHotel()

--- Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/services/HotelServiceImpl.java
+++ Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/services/HotelServiceImpl.java
@@ -1,9 +1,11 @@
 package com.nestigo.systemdesign.nestigo.services;
 
 import com.nestigo.systemdesign.nestigo.dtos.HotelDTO;
+import com.nestigo.systemdesign.nestigo.entities.HotelEntity;
 import com.nestigo.systemdesign.nestigo.repositories.HotelRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -13,11 +15,19 @@ public class HotelServiceImpl implements HotelService{
 
 
     private final HotelRepository hotelRepository;
+    private final ModelMapper modelMapper;
 
 
     @Override
     public HotelDTO createHotel(HotelDTO hotelDTO) {
         log.info("Creating a new hotel with name: {}", hotelDTO.getName());
+
+        //converting dto to entity
+        HotelEntity hotelEntity = modelMapper.map(hotelDTO,HotelEntity.class);
+        hotelEntity.setActive(false);
+        hotelEntity = hotelRepository.save(hotelEntity);
+
+
 
         return null;
     }
