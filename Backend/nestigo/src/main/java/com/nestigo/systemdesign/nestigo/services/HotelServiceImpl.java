bc22017a7997b25573d93855fc734f3b1ae2accf
Umair Ali <umairmamoor@gmail.com>
2025-07-02 01:54:35 -0400

    Hotel Service and its implementation

---
+++ Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/services/HotelServiceImpl.java
@@ -0,0 +1,11 @@
+package com.nestigo.systemdesign.nestigo.services;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@RequiredArgsConstructor
+public class HotelServiceImpl implements HotelService{
+}
