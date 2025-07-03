5a563b1a08ee2ae1f2e7c04662b7445f3ca52372
Umair Ali <umairmamoor@gmail.com>
2025-07-03 02:41:04 -0400

    roomService interface

--- Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/services/RoomService.java
+++ Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/services/RoomService.java
@@ -1,6 +1,20 @@
 package com.nestigo.systemdesign.nestigo.services;
 
+import com.nestigo.systemdesign.nestigo.dtos.RoomDTO;
+
+import java.util.List;
+
 public interface RoomService {
+
+    RoomDTO createRoom (RoomDTO roomDTO);
+
+    List<RoomDTO> getAllRoomsByHotel (Long hotelIDd);
+
+    RoomDTO getRoomById(Long id);
+
+    void deleteRoomById(Long roomId);
+
+
 
 
 }
