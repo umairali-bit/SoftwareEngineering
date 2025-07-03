aa075b8970f1007cd09918f6b6fbe4ac07a626ed
Umair Ali <umairmamoor@gmail.com>
2025-07-03 03:47:40 -0400

    creating a new room in a hotel

--- Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/services/RoomServiceImpl.java
+++ Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/services/RoomServiceImpl.java
@@ -1,9 +1,14 @@
 package com.nestigo.systemdesign.nestigo.services;
 
 import com.nestigo.systemdesign.nestigo.dtos.RoomDTO;
+import com.nestigo.systemdesign.nestigo.entities.HotelEntity;
+import com.nestigo.systemdesign.nestigo.entities.RoomEntity;
+import com.nestigo.systemdesign.nestigo.exceptions.ResourceNotFoundException;
+import com.nestigo.systemdesign.nestigo.repositories.HotelRepository;
 import com.nestigo.systemdesign.nestigo.repositories.RoomRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -15,11 +20,29 @@ import java.util.List;
 public class RoomServiceImpl implements RoomService{
 
     private final RoomRepository roomRepository;
+    private final HotelRepository hotelRepository;
+    private final ModelMapper modelMapper;
 
 
     @Override
-    public RoomDTO createRoom(RoomDTO roomDTO) {
-        return null;
+    public RoomDTO createRoom(Long hotelId, RoomDTO roomDTO) {
+        log.info("Creating a room in hotel with ID: {}", hotelId);
+
+
+    //  checking if the hotel exists or not
+        HotelEntity hotel = hotelRepository
+                .findById(hotelId)
+                .orElseThrow(() -> new ResourceNotFoundException("Hotel NOT found with ID: " + hotelId));
+
+        RoomEntity room = modelMapper.map(roomDTO, RoomEntity.class);
+        room.setHotel(hotel);
+        room = roomRepository.save(room);
+
+
+
+        return modelMapper.map(room, RoomDTO.class);
+
+    //    TODO: add inventory
     }
 
     @Override
