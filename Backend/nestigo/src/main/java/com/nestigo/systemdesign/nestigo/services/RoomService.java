363934c65c1444c8a12d2e59669fc9c387a62e6f
Umair Ali <umairmamoor@gmail.com>
2025-07-03 03:47:40 -0400

    creating a new room in a hotel

--- Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/services/RoomService.java
+++ Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/services/RoomService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface RoomService {
 
-    RoomDTO createRoom (RoomDTO roomDTO);
+    RoomDTO createRoom (Long hotelId, RoomDTO roomDTO);
 
     List<RoomDTO> getAllRoomsByHotel (Long hotelIDd);
 
