2dfbeb24fc3eea5aac89f012072ac5abcc61a9e6
Umair Ali <umairmamoor@gmail.com>
2025-07-04 08:19:24 -0400

    manipulating inventories in the project

--- Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/repositories/InventoryRepository.java
+++ Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/repositories/InventoryRepository.java
@@ -1,10 +1,15 @@
 package com.nestigo.systemdesign.nestigo.repositories;
 
 import com.nestigo.systemdesign.nestigo.entities.InventoryEntity;
+import com.nestigo.systemdesign.nestigo.entities.RoomEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
+
 @Repository
 public interface InventoryRepository extends JpaRepository<InventoryEntity, Long> {
+
+    void deleteRoomByDateAfter(LocalDate date, RoomEntity room);
 
 }
