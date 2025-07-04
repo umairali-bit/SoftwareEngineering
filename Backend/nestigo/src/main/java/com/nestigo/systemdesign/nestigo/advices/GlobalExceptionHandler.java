786b75aca807612b139a8a6db865e393326593f9
Umair Ali <umairmamoor@gmail.com>
2025-07-04 08:55:59 -0400

    fixing the error messages

--- Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/advices/GlobalExceptionHandler.java
+++ Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/advices/GlobalExceptionHandler.java
@@ -1,14 +1,11 @@
 package com.nestigo.systemdesign.nestigo.advices;
 
+
 import com.nestigo.systemdesign.nestigo.exceptions.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import java.util.List;
-import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
@@ -26,23 +23,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ApiResponse<?>> handleInternalServerError(Exception exception) {
         ApiError apiError = ApiError.builder()
                 .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                .message("An unexpected error occurred")
-                .build();
-        return buildErrorResponseEntity(apiError);
-    }
-
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ApiResponse<?>> handleValidationErrors(MethodArgumentNotValidException exception) {
-        List<String> errors = exception.getBindingResult()
-                .getAllErrors()
-                .stream()
-                .map(error -> error.getDefaultMessage())
-                .collect(Collectors.toList());
-
-        ApiError apiError = ApiError.builder()
-                .status(HttpStatus.BAD_REQUEST)
-                .message("Input validation failed")
-                .subErrors(errors)
+                .message(exception.getMessage())
                 .build();
         return buildErrorResponseEntity(apiError);
     }
@@ -50,6 +31,7 @@ public class GlobalExceptionHandler {
     private ResponseEntity<ApiResponse<?>> buildErrorResponseEntity(ApiError apiError) {
         return new ResponseEntity<>(new ApiResponse<>(apiError), apiError.getStatus());
     }
+
 }
 
 
