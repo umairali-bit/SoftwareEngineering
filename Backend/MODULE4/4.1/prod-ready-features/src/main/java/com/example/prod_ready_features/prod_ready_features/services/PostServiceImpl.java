336261ea7d119d0a1321cb8d144a08477410ff5d
Umair Ali <umairmamoor@gmail.com>
2025-07-05 03:04:30 -0400

    adding modelMapper dependency

--- Backend/MODULE4/4.1/prod-ready-features/src/main/java/com/example/prod_ready_features/prod_ready_features/services/PostServiceImpl.java
+++ Backend/MODULE4/4.1/prod-ready-features/src/main/java/com/example/prod_ready_features/prod_ready_features/services/PostServiceImpl.java
@@ -1,13 +1,21 @@
 package com.example.prod_ready_features.prod_ready_features.services;
 
 import com.example.prod_ready_features.prod_ready_features.dtos.PostDTO;
+import com.example.prod_ready_features.prod_ready_features.repositories.PostRepository;
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 
 @Service
+@RequiredArgsConstructor
 public class PostServiceImpl implements PostService{
+
+    private final PostRepository postRepository;
+
+
 
 
     @Override
