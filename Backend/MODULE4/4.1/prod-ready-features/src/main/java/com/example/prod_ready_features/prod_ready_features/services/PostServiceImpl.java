2d41042065d5a9a398572e01510022f9454d695b
Umair Ali <umairmamoor@gmail.com>
2025-07-05 02:18:34 -0400

    postServiceImpl

---
+++ Backend/MODULE4/4.1/prod-ready-features/src/main/java/com/example/prod_ready_features/prod_ready_features/services/PostServiceImpl.java
@@ -0,0 +1,22 @@
+package com.example.prod_ready_features.prod_ready_features.services;
+
+import com.example.prod_ready_features.prod_ready_features.dtos.PostDTO;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+@Service
+public class PostServiceImpl implements PostService{
+
+
+    @Override
+    public List<PostDTO> getAllPosts() {
+        return null;
+    }
+
+    @Override
+    public PostDTO createNewPost(PostDTO postDTO) {
+        return null;
+    }
+}
