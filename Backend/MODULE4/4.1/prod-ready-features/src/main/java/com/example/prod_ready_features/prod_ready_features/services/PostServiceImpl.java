83d8a7c6187cc405754511136876ad058dfe000b
Umair Ali <umairmamoor@gmail.com>
2025-07-06 17:15:26 -0400

    getAllPosts()

--- Backend/MODULE4/4.1/prod-ready-features/src/main/java/com/example/prod_ready_features/prod_ready_features/services/PostServiceImpl.java
+++ Backend/MODULE4/4.1/prod-ready-features/src/main/java/com/example/prod_ready_features/prod_ready_features/services/PostServiceImpl.java
@@ -2,24 +2,31 @@ package com.example.prod_ready_features.prod_ready_features.services;
 
 import com.example.prod_ready_features.prod_ready_features.dtos.PostDTO;
 import com.example.prod_ready_features.prod_ready_features.repositories.PostRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @Service
 public class PostServiceImpl implements PostService{
 
     private final PostRepository postRepository;
+    private final ModelMapper modelMapper;
 
-    public PostServiceImpl(PostRepository postRepository) {
+    public PostServiceImpl(PostRepository postRepository, ModelMapper modelMapper) {
         this.postRepository = postRepository;
+        this.modelMapper = modelMapper;
     }
 
 
     @Override
     public List<PostDTO> getAllPosts() {
-        return null;
+        return postRepository.findAll()
+                .stream()
+                .map(postEntity -> modelMapper.map(postEntity, PostDTO.class))
+                .collect(Collectors.toList());
     }
 
     @Override
