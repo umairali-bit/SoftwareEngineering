3727543183a5183e7a5f97dd934986582e394b80
Umair Ali <umairmamoor@gmail.com>
2025-07-06 17:24:49 -0400

    postController

---
+++ Backend/MODULE4/4.1/prod-ready-features/src/main/java/com/example/prod_ready_features/prod_ready_features/controllers/PostController.java
@@ -0,0 +1,34 @@
+package com.example.prod_ready_features.prod_ready_features.controllers;
+
+
+import com.example.prod_ready_features.prod_ready_features.dtos.PostDTO;
+import com.example.prod_ready_features.prod_ready_features.services.PostService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/posts")
+public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+
+    @GetMapping
+    public List<PostDTO> getAllPosts() {
+        return postService.getAllPosts();
+
+    }
+
+    @PostMapping
+    public PostDTO createNewPost (@RequestBody PostDTO postDTO) {
+        return postService.createNewPost(postDTO);
+
+    }
+
+
+}
