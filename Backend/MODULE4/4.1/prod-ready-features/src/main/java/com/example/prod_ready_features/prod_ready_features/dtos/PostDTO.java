69a0d94fca73b4cdee80f32f65ef8622cda95754
Umair Ali <umairmamoor@gmail.com>
2025-07-06 17:52:01 -0400

    trouble shooting

--- Backend/MODULE4/4.1/prod-ready-features/src/main/java/com/example/prod_ready_features/prod_ready_features/dtos/PostDTO.java
+++ Backend/MODULE4/4.1/prod-ready-features/src/main/java/com/example/prod_ready_features/prod_ready_features/dtos/PostDTO.java
@@ -1,15 +1,14 @@
 package com.example.prod_ready_features.prod_ready_features.dtos;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.web.bind.annotation.DeleteMapping;
 
-@Data
+@Data  // This adds getters, setters, toString, equals, and hashCode
 @NoArgsConstructor
 @AllArgsConstructor
 public class PostDTO {
     private String title;
     private String description;
 }
+
