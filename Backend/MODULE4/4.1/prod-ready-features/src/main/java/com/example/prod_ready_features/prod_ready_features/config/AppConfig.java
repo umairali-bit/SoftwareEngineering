955090dd312e9988422293a0858a2942e03a3eb9
Umair Ali <umairmamoor@gmail.com>
2025-07-06 17:12:53 -0400

    adding model mapper

---
+++ Backend/MODULE4/4.1/prod-ready-features/src/main/java/com/example/prod_ready_features/prod_ready_features/config/AppConfig.java
@@ -0,0 +1,16 @@
+package com.example.prod_ready_features.prod_ready_features.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+
+    @Bean
+    ModelMapper getModelMapper() {
+        return new ModelMapper();
+    }
+
+
+}
