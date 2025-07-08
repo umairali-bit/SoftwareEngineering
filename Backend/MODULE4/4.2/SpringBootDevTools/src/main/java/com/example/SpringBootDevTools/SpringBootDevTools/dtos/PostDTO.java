bbe4bab8267da423789a0194bcb0f86a189ffb8a
Umair Ali <umairmamoor@gmail.com>
2025-07-08 02:15:24 -0400

    Spring Boot DevTools

--- Backend/MODULE4/4.2/SpringBootDevTools/src/main/java/com/example/SpringBootDevTools/SpringBootDevTools/dtos/PostDTO.java
+++ Backend/MODULE4/4.2/SpringBootDevTools/src/main/java/com/example/SpringBootDevTools/SpringBootDevTools/dtos/PostDTO.java
@@ -1,25 +1,16 @@
 package com.example.SpringBootDevTools.SpringBootDevTools.dtos;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PostDTO {
     private Long id;
     private String title;
     private String description;
 
-    public PostDTO() {}
-
-    public PostDTO(Long id, String title, String description) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-    }
-
-    public Long getId() { return id; }
-    public void setId(Long id) { this.id = id; }
-
-    public String getTitle() { return title; }
-    public void setTitle(String title) { this.title = title; }
-
-    public String getDescription() { return description; }
-    public void setDescription(String description) { this.description = description; }
 }
 
