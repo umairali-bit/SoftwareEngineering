3bf4043a7c4d3e1e2af4f12445b1d2c9e74e92e1
Umair Ali <umairmamoor@gmail.com>
2025-07-08 02:50:55 -0400

    adding a Auditable class

---
+++ Backend/MODULE4/4.3/Auditing/src/main/java/com/example/Auditing/Auditing/entities/AuditableEntity.java
@@ -0,0 +1,35 @@
+package com.example.Auditing.Auditing.entities;
+
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass
+@Getter
+@Setter
+@EntityListeners(AuditingEntityListener.class)
+public class AuditableEntity {
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private LocalDateTime createDate;
+
+    @LastModifiedDate
+    private LocalDateTime updatedDate;
+
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedBy
+    private String updatedBy;
+
+}
