33f19f3b75f814382696fc1669850a724f7cacce
Umair Ali <umairmamoor@gmail.com>
2025-07-09 03:53:07 -0400

    RestClient configuration

--- Backend/MODULE4/4.4/ThirdPartyAPICallsWithRestClient/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/config/RestClientConfig.java
+++ Backend/MODULE4/4.4/ThirdPartyAPICallsWithRestClient/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/config/RestClientConfig.java
@@ -1,2 +1,30 @@
-package com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.config;public class RestClientConfig {
+package com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.config;
+
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestClient;
+
+import static org.springframework.http.HttpHeaders.CONTENT_TYPE;
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@Configuration
+public class RestClientConfig {
+
+    @Value("${employeeService.base.url}")
+    private String BASE_URL;
+
+    @Bean
+    @Qualifier("employeeRestClient")
+    RestClient getEmployeeServiceRestClient() {
+        return RestClient.builder()
+                .baseUrl(BASE_URL)
+                .defaultHeader(CONTENT_TYPE, APPLICATION_JSON_VALUE)
+                .build();
+
+
+    }
+
 }
