2db5d110bcf9ddae79c5aef11608258434d212c4
Umair Ali <umairmamoor@gmail.com>
2025-07-09 02:39:18 -0400

    Making third part API calls with RestClient

---
+++ Backend/MODULE4/4.4/ThirdPartyAPICallsWithRestClient/src/test/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/ProdReadyFeaturesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdReadyFeaturesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
