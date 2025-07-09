fae27a7b4ec4247a8fecc0da208407ed3b068c25
Umair Ali <umairmamoor@gmail.com>
2025-07-09 03:23:08 -0400

    restructuring the project

--- Backend/MODULE2/2.7/exceptionhandelingInSpringBoot/src/main/java/com/exceptionHandelingInSpringBoot/ExceptionhandelingInSpringBootApplication.java
+++ Backend/MODULE2/2.7/exceptionhandelingInSpringBoot/src/main/java/com/exceptionHandelingInSpringBoot/ExceptionhandelingInSpringBootApplication.java
@@ -1,4 +1,4 @@
-package com.example.exceptionHandelingInSpringBoot;
+package com.exceptionHandelingInSpringBoot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,5 +9,4 @@ public class ExceptionhandelingInSpringBootApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ExceptionhandelingInSpringBootApplication.class, args);
 	}
-
 }
---
+++ Backend/MODULE4/4.4/ThirdPartyAPICallsWithRestClient/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/config/RestClientConfig.java
@@ -0,0 +1,2 @@
+package com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.config;public class RestClientConfig {
+}
