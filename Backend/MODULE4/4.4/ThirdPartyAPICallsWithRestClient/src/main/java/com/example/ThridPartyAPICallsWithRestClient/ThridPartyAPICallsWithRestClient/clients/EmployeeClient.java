3c022196b5c8756cb799a0042d32640a058add58
Umair Ali <umairmamoor@gmail.com>
2025-07-10 11:19:23 -0400

    EmployeeDTO

--- Backend/MODULE4/4.4/ThirdPartyAPICallsWithRestClient/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/clients/EmployeeClient.java
+++ Backend/MODULE4/4.4/ThirdPartyAPICallsWithRestClient/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/clients/EmployeeClient.java
@@ -1,5 +1,9 @@
 package com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.clients;
 
+import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.dtos.EmployeeDTO;
+
+import java.util.List;
+
 public interface EmployeeClient {
 
     List<EmployeeDTO> getAllEmployees();
