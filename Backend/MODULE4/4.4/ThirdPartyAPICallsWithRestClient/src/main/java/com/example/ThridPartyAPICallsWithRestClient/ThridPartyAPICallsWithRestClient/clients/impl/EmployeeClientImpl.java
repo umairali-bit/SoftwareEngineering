e78af8ee31a3601eea7081350bd8cceb6725ee6d
Umair Ali <umairmamoor@gmail.com>
2025-07-10 11:26:09 -0400

    EmployeeClientImplementation

---
+++ Backend/MODULE4/4.4/ThirdPartyAPICallsWithRestClient/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/clients/impl/EmployeeClientImpl.java
@@ -0,0 +1,13 @@
+package com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.clients.impl;
+
+import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.clients.EmployeeClient;
+import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.dtos.EmployeeDTO;
+
+import java.util.List;
+
+public class EmployeeClientImpl implements EmployeeClient {
+    @Override
+    public List<EmployeeDTO> getAllEmployees() {
+        return null;
+    }
+}
