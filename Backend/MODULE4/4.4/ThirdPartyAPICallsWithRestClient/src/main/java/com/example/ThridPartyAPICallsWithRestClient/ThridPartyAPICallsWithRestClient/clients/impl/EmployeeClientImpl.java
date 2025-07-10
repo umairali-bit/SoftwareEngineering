cedfb5be8c7906002896bc394f0db99fda886ee3
Umair Ali <umairmamoor@gmail.com>
2025-07-10 11:35:54 -0400

    getting all employees from a thrid party client

--- Backend/MODULE4/4.4/ThirdPartyAPICallsWithRestClient/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/clients/impl/EmployeeClientImpl.java
+++ Backend/MODULE4/4.4/ThirdPartyAPICallsWithRestClient/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/clients/impl/EmployeeClientImpl.java
@@ -2,12 +2,26 @@ package com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestC
 
 import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.clients.EmployeeClient;
 import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.dtos.EmployeeDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.web.client.RestClient;
 
 import java.util.List;
 
+
+@RequiredArgsConstructor
 public class EmployeeClientImpl implements EmployeeClient {
+
+    private final RestClient restClient;
+
     @Override
     public List<EmployeeDTO> getAllEmployees() {
-        return null;
+        List<EmployeeDTO> employeeDTO = restClient.get()
+                .uri("employees")
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {
+                });
+
+        return employeeDTO;
     }
 }
