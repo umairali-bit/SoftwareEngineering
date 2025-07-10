e54dbac205249d3efa8a00d4f1fe698c84eb7bbf
Umair Ali <umairmamoor@gmail.com>
2025-07-10 11:19:23 -0400

    EmployeeDTO

--- Backend/MODULE4/4.4/ThirdPartyAPICallsWithRestClient/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/dtos/EmployeeDTO.java
+++ Backend/MODULE4/4.4/ThirdPartyAPICallsWithRestClient/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/dtos/EmployeeDTO.java
@@ -1,2 +1,34 @@
-package com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.dtos;public class EmployeeDTO {
+package com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class EmployeeDTO {
+
+    private Long id;
+
+    private String name;
+
+    private String email;
+
+
+    private Integer age;
+
+
+    private String role;
+
+    private LocalDate dateOfJoining;
+
+    private Boolean isActive;
+
 }
