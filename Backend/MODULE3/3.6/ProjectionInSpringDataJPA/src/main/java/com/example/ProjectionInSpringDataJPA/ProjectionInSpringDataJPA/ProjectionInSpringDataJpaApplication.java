9f1c45c24e6ded108be020520604a2e1c64f1237
Umair Ali <umairmamoor@gmail.com>
2025-07-22 03:44:42 -0400

    Projection In SpringDataJPA

---
+++ Backend/MODULE3/3.6/ProjectionInSpringDataJPA/src/main/java/com/example/ProjectionInSpringDataJPA/ProjectionInSpringDataJPA/ProjectionInSpringDataJpaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProjectionInSpringDataJPA.ProjectionInSpringDataJPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectionInSpringDataJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectionInSpringDataJpaApplication.class, args);
+	}
+
+}
