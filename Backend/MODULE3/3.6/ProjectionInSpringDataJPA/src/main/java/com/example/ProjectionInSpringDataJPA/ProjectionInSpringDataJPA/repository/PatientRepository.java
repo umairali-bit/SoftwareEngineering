8ba9acb66b1b223a20177822750c593f85300d23
Umair Ali <umairmamoor@gmail.com>
2025-07-22 04:12:23 -0400

    PateintRepository

---
+++ Backend/MODULE3/3.6/ProjectionInSpringDataJPA/src/main/java/com/example/ProjectionInSpringDataJPA/ProjectionInSpringDataJPA/repository/PatientRepository.java
@@ -0,0 +1,10 @@
+package com.example.ProjectionInSpringDataJPA.ProjectionInSpringDataJPA.repository;
+
+import com.example.ProjectionInSpringDataJPA.ProjectionInSpringDataJPA.entity.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    List<Patient> findByEmailContaining(String d);
+}
