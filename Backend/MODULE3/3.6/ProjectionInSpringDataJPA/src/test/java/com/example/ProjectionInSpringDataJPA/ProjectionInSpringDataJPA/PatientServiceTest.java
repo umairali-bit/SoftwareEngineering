3ffed494d94637c67ec1b10dd676c19b65f0fb90
Umair Ali <umairmamoor@gmail.com>
2025-07-23 01:49:46 -0400

    testing patients

--- Backend/MODULE3/3.6/ProjectionInSpringDataJPA/src/test/java/com/example/ProjectionInSpringDataJPA/ProjectionInSpringDataJPA/PatientServiceTest.java
+++ Backend/MODULE3/3.6/ProjectionInSpringDataJPA/src/test/java/com/example/ProjectionInSpringDataJPA/ProjectionInSpringDataJPA/PatientServiceTest.java
@@ -1,10 +1,13 @@
 package com.example.ProjectionInSpringDataJPA.ProjectionInSpringDataJPA;
 
 
+import com.example.ProjectionInSpringDataJPA.ProjectionInSpringDataJPA.entity.Patient;
 import com.example.ProjectionInSpringDataJPA.ProjectionInSpringDataJPA.repository.PatientRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 public class PatientServiceTest {
@@ -14,6 +17,12 @@ public class PatientServiceTest {
 
     @Test
     public void testPatient() {
+
+        List<Patient> patientList = patientRepository.findAll();
+
+        for (Patient p : patientList) {
+            System.out.println(p);
+        }
 
 
     }
