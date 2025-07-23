b31fcd4dcfd8d264a3b8c4f2e25a5e4db2cdfd9f
Umair Ali <umairmamoor@gmail.com>
2025-07-23 01:53:55 -0400

    creating a dto interface

--- Backend/MODULE3/3.6/ProjectionInSpringDataJPA/src/main/java/com/example/ProjectionInSpringDataJPA/ProjectionInSpringDataJPA/repository/PatientRepository.java
+++ Backend/MODULE3/3.6/ProjectionInSpringDataJPA/src/main/java/com/example/ProjectionInSpringDataJPA/ProjectionInSpringDataJPA/repository/PatientRepository.java
@@ -1,10 +1,13 @@
 package com.example.ProjectionInSpringDataJPA.ProjectionInSpringDataJPA.repository;
 
+import com.example.ProjectionInSpringDataJPA.ProjectionInSpringDataJPA.dto.PatientInfo;
 import com.example.ProjectionInSpringDataJPA.ProjectionInSpringDataJPA.entity.Patient;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface PatientRepository extends JpaRepository<Patient, Long> {
-    List<Patient> findByEmailContaining(String d);
+    @Query("select p.id, p.name, p.email from Patient p")
+    List<PatientInfo> getPatientInfo();
 }
