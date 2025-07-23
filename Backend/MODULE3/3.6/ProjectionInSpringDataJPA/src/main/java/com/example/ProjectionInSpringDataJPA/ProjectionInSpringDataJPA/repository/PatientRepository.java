4e15754e3d5f2824b336ae44e2b521c8b96b0a47
Umair Ali <umairmamoor@gmail.com>
2025-07-23 02:34:56 -0400

    how to modify a query

--- Backend/MODULE3/3.6/ProjectionInSpringDataJPA/src/main/java/com/example/ProjectionInSpringDataJPA/ProjectionInSpringDataJPA/repository/PatientRepository.java
+++ Backend/MODULE3/3.6/ProjectionInSpringDataJPA/src/main/java/com/example/ProjectionInSpringDataJPA/ProjectionInSpringDataJPA/repository/PatientRepository.java
@@ -4,8 +4,11 @@ import com.example.ProjectionInSpringDataJPA.ProjectionInSpringDataJPA.dto.Blood
 import com.example.ProjectionInSpringDataJPA.ProjectionInSpringDataJPA.dto.PatientInfo;
 import com.example.ProjectionInSpringDataJPA.ProjectionInSpringDataJPA.dto.PatientInfoImpl;
 import com.example.ProjectionInSpringDataJPA.ProjectionInSpringDataJPA.entity.Patient;
+import jakarta.transaction.Transactional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -22,4 +25,10 @@ public interface PatientRepository extends JpaRepository<Patient, Long> {
     @Query("select new com.example.ProjectionInSpringDataJPA.ProjectionInSpringDataJPA.dto.BloodGroupStats(p.bloodGroup," +
             "COUNT(p)) from Patient p group by p.bloodGroup order by COUNT(p)")
     List<BloodGroupStats> getBloodGroupStats();
+
+
+    @Transactional //either all the transactions will occur or none of them will occur
+    @Modifying
+    @Query("UPDATE Patient p set p.name = :name where p.id = :id")
+    int updatePatientNameWithId(@Param("name") String name, @Param("id") Long id);
 }
