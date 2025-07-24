8326d035e338026c22dc1487223aa173555f55d8
Umair Ali <umairmamoor@gmail.com>
2025-07-24 03:17:44 -0400

    OneToOne, OneToMany, ManyToOne, ManyToMany mappings in Spring Data Jpa

---
+++ Backend/MODULE3/3.8/ProjectionInSpringDataJPA/src/main/java/com/example/ProjectionInSpringDataJPA/ProjectionInSpringDataJPA/entity/type/BloodGroup.java
@@ -0,0 +1,12 @@
+package com.example.ProjectionInSpringDataJPA.ProjectionInSpringDataJPA.entity.type;
+
+public enum BloodGroup {
+    A_POSITIVE,
+    A_NEGATIVE,
+    B_POSITIVE,
+    B_NEGATIVE,
+    AB_POSITIVE,
+    AB_NEGATIVE,
+    O_POSITIVE,
+    O_NEGATIVE;
+}
---
+++ Backend/MODULE3/3.8/ProjectionInSpringDataJPA/src/test/java/com/example/ProjectionInSpringDataJPA/ProjectionInSpringDataJPA/PatientServiceTest.java
@@ -0,0 +1,58 @@
+package com.example.ProjectionInSpringDataJPA.ProjectionInSpringDataJPA;
+
+
+import com.example.ProjectionInSpringDataJPA.ProjectionInSpringDataJPA.dto.BloodGroupStats;
+import com.example.ProjectionInSpringDataJPA.ProjectionInSpringDataJPA.dto.PatientInfo;
+import com.example.ProjectionInSpringDataJPA.ProjectionInSpringDataJPA.dto.PatientInfoImpl;
+import com.example.ProjectionInSpringDataJPA.ProjectionInSpringDataJPA.entity.Patient;
+import com.example.ProjectionInSpringDataJPA.ProjectionInSpringDataJPA.repository.PatientRepository;
+import com.example.ProjectionInSpringDataJPA.ProjectionInSpringDataJPA.service.PatientService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
+public class PatientServiceTest {
+
+    @Autowired
+    private PatientService patientService;
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    @Test
+    public void testPatient() {
+
+        // List<Patient> patientList = patientRepository.findAll();
+
+        List<PatientInfo> patientList = patientRepository.getPatientInfo();
+
+        for (PatientInfo p : patientList) {
+            System.out.println(p);
+        }
+
+        List<PatientInfoImpl> patientInfos = patientRepository.getPatientInfoConceret();
+
+        for (PatientInfoImpl a : patientInfos) {
+            System.out.println(a);
+        }
+
+        List<BloodGroupStats> bloodGroupStats = patientRepository.getBloodGroupStats();
+
+        for (var n : bloodGroupStats) {
+            System.out.println(n);
+        }
+
+        int rowsAffected = patientRepository.updatePatientNameWithId("Umair Ali", 1L);
+        System.out.println(rowsAffected);
+
+        patientService.testPatientTransaction();
+
+
+
+
+
+    }
+}
