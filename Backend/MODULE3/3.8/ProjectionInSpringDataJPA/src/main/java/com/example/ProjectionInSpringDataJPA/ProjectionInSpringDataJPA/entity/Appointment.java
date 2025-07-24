738ae07ee883d0b9911e80058e94081669414b50
Umair Ali <umairmamoor@gmail.com>
2025-07-24 03:58:03 -0400

    Appointment table

---
+++ Backend/MODULE3/3.8/ProjectionInSpringDataJPA/src/main/java/com/example/ProjectionInSpringDataJPA/ProjectionInSpringDataJPA/entity/Appointment.java
@@ -0,0 +1,74 @@
+package com.example.ProjectionInSpringDataJPA.ProjectionInSpringDataJPA.entity;
+
+
+import jakarta.persistence.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Appointment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private LocalDateTime appointmentTime;
+
+    private String reason;
+
+    @ManyToOne//owning
+    @JoinColumn(nullable = false)
+    private Patient patient;
+
+    public Appointment() {
+    }
+
+    public Appointment(Long id, LocalDateTime appointmentTime, String reason, Patient patient) {
+        this.id = id;
+        this.appointmentTime = appointmentTime;
+        this.reason = reason;
+        this.patient = patient;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getAppointmentTime() {
+        return appointmentTime;
+    }
+
+    public void setAppointmentTime(LocalDateTime appointmentTime) {
+        this.appointmentTime = appointmentTime;
+    }
+
+    public String getReason() {
+        return reason;
+    }
+
+    public void setReason(String reason) {
+        this.reason = reason;
+    }
+
+    public Patient getPatient() {
+        return patient;
+    }
+
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    @Override
+    public String toString() {
+        return "Appointment{" +
+                "id=" + id +
+                ", appointmentTime=" + appointmentTime +
+                ", reason='" + reason + '\'' +
+                ", patient=" + patient +
+                '}';
+    }
+}
