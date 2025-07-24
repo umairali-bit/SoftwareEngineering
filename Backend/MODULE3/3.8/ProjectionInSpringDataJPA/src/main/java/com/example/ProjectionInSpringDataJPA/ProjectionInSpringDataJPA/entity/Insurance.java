c6c893a78317cf29d8fc94b3f7c59026954582d8
Umair Ali <umairmamoor@gmail.com>
2025-07-24 03:25:56 -0400

    Insurance Entity

--- Backend/MODULE3/3.8/ProjectionInSpringDataJPA/src/main/java/com/example/ProjectionInSpringDataJPA/ProjectionInSpringDataJPA/entity/Insurance.java
+++ Backend/MODULE3/3.8/ProjectionInSpringDataJPA/src/main/java/com/example/ProjectionInSpringDataJPA/ProjectionInSpringDataJPA/entity/Insurance.java
@@ -27,5 +27,56 @@ public class Insurance {
     @Column(nullable = false, updatable = false)
     private LocalDateTime createdAt;
 
+    public Insurance() {
+    }
+
+    public Insurance(Long id, String policyNumber, String provider, LocalDate validUntil, LocalDateTime createdAt) {
+        this.id = id;
+        this.policyNumber = policyNumber;
+        this.provider = provider;
+        this.validUntil = validUntil;
+        this.createdAt = createdAt;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPolicyNumber() {
+        return policyNumber;
+    }
+
+    public void setPolicyNumber(String policyNumber) {
+        this.policyNumber = policyNumber;
+    }
+
+    public String getProvider() {
+        return provider;
+    }
+
+    public void setProvider(String provider) {
+        this.provider = provider;
+    }
+
+    public LocalDate getValidUntil() {
+        return validUntil;
+    }
+
+    public void setValidUntil(LocalDate validUntil) {
+        this.validUntil = validUntil;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
 
 }
