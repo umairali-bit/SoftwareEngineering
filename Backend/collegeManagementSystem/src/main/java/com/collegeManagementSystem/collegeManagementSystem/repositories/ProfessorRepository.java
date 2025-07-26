ebad7c71db58bb5ab9d40ae385be395143691348
Umair Ali <umairmamoor@gmail.com>
2025-07-26 02:18:02 -0400

    Delete Backend/collegeManagementSystem directory

--- Backend/collegeManagementSystem/src/main/java/com/collegeManagementSystem/collegeManagementSystem/repositories/ProfessorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.collegeManagementSystem.collegeManagementSystem.repositories;
-
-import com.collegeManagementSystem.collegeManagementSystem.entities.ProfessorEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProfessorRepository extends JpaRepository<ProfessorEntity, Long> {
-}
--- Backend/collegeManagementSystem/src/main/java/com/collegeManagementSystem/collegeManagementSystem/repositories/SubjectRepository.java
+++
@@ -1,7 +0,0 @@
-package com.collegeManagementSystem.collegeManagementSystem.repositories;
-
-import com.collegeManagementSystem.collegeManagementSystem.entities.SubjectEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SubjectRepository extends JpaRepository<SubjectEntity, Long> {
-}
