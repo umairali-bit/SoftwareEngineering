a945fd844e012896efba999033cf9e4f44ef0ab7
Umair Ali <umairmamoor@gmail.com>
2025-07-31 13:38:54 -0400

    AdmissionRecordDTO

--- Backend/CollegeManagementSystem/src/main/java/com/example/CollegeManagementSystem/CollegeManagementSystem/dtos/AdmissionRecordDTO.java
+++ Backend/CollegeManagementSystem/src/main/java/com/example/CollegeManagementSystem/CollegeManagementSystem/dtos/AdmissionRecordDTO.java
@@ -1,4 +1,23 @@
 package com.example.CollegeManagementSystem.CollegeManagementSystem.dtos;
 
+
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class AdmissionRecordDTO {
+
+    private Long id;
+
+    private Double fees;
+
+    private Long studentId;
+
+    private String studentName;
+
+
+
 }
