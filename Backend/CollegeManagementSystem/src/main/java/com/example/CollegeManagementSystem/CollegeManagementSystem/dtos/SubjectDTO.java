97e21fa742099041bbba4d317d9f9bdecf12ade8
Umair Ali <umairmamoor@gmail.com>
2025-08-21 03:27:36 -0400

    advices

--- Backend/CollegeManagementSystem/src/main/java/com/example/CollegeManagementSystem/CollegeManagementSystem/dtos/SubjectDTO.java
+++ Backend/CollegeManagementSystem/src/main/java/com/example/CollegeManagementSystem/CollegeManagementSystem/dtos/SubjectDTO.java
@@ -1,5 +1,6 @@
 package com.example.CollegeManagementSystem.CollegeManagementSystem.dtos;
 
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.*;
 
 import java.util.HashSet;
