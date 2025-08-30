2490b341fa3bc5f8f976d1724d940250b1aa1cf4
Umair Ali <umairmamoor@gmail.com>
2025-08-30 03:13:23 -0400

    PostController

--- Backend/CollegeManagementSystem/src/main/java/com/example/CollegeManagementSystem/CollegeManagementSystem/controllers/ProfessorController.java
+++ Backend/CollegeManagementSystem/src/main/java/com/example/CollegeManagementSystem/CollegeManagementSystem/controllers/ProfessorController.java
@@ -1,8 +1,14 @@
 package com.example.CollegeManagementSystem.CollegeManagementSystem.controllers;
 
 
+import com.example.CollegeManagementSystem.CollegeManagementSystem.dtos.ProfessorDTO;
+import com.example.CollegeManagementSystem.CollegeManagementSystem.dtos.StudentDTO;
 import com.example.CollegeManagementSystem.CollegeManagementSystem.services.ProfessorService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +18,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProfessorController {
 
     private final ProfessorService professorService;
+
+    //create a new professor
+    @PostMapping
+    public ResponseEntity<ProfessorDTO> createProfessor(@RequestBody ProfessorDTO professorDTO) {
+
+        ProfessorDTO created = professorService.createProfessor(professorDTO);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+
+    }
+
+
 
 
 }
