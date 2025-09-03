c17d97f3d74a0e1046bd6a74c249ea2f9a9bf742
Umair Ali <umairmamoor@gmail.com>
2025-09-03 01:57:12 -0400

    services

--- Backend/LibraryManagementSystem/src/main/java/com/example/libraryManagementSystem/LibraryManagementSystem/repositories/AuthorRepository.java
+++ Backend/LibraryManagementSystem/src/main/java/com/example/libraryManagementSystem/LibraryManagementSystem/repositories/AuthorRepository.java
@@ -1,4 +1,8 @@
 package com.example.libraryManagementSystem.LibraryManagementSystem.repositories;
 
-public interface AuthorRepository {
+import com.example.libraryManagementSystem.LibraryManagementSystem.entities.AuthorEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AuthorRepository extends JpaRepository<AuthorEntity,Long> {
+
 }
---
+++ Backend/LibraryManagementSystem/src/main/java/com/example/libraryManagementSystem/LibraryManagementSystem/services/BookService.java
@@ -0,0 +1,4 @@
+package com.example.libraryManagementSystem.LibraryManagementSystem.services;
+
+public class BookService {
+}
