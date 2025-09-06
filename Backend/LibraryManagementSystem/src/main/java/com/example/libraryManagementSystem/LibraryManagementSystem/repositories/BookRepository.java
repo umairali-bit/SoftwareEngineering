f680dac83c32036179d1ae7c43ef98963c5f34ff
Umair Ali <umairmamoor@gmail.com>
2025-09-06 01:53:18 -0400

    findBookPublishedAfter()

--- Backend/LibraryManagementSystem/src/main/java/com/example/libraryManagementSystem/LibraryManagementSystem/repositories/BookRepository.java
+++ Backend/LibraryManagementSystem/src/main/java/com/example/libraryManagementSystem/LibraryManagementSystem/repositories/BookRepository.java
@@ -5,10 +5,13 @@ import com.example.libraryManagementSystem.LibraryManagementSystem.entities.Book
 import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
 public interface BookRepository extends JpaRepository<BookEntity,Long> {
     @EntityGraph(attributePaths = "author")
     Optional<BookEntity> findByTitle(String title);
+
+    List<BookEntity> findByPublishedDateAfter(LocalDateTime date);
 }
