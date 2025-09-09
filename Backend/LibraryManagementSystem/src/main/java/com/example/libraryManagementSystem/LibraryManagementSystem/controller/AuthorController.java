63fb8e79f82361833cec5ba0c545fede50bfd743
Umair Ali <umairmamoor@gmail.com>
2025-09-09 01:58:06 -0400

    getAllAuthors controller

--- Backend/LibraryManagementSystem/src/main/java/com/example/libraryManagementSystem/LibraryManagementSystem/controller/AuthorController.java
+++ Backend/LibraryManagementSystem/src/main/java/com/example/libraryManagementSystem/LibraryManagementSystem/controller/AuthorController.java
@@ -7,10 +7,9 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/authors")
@@ -28,6 +27,14 @@ public class AuthorController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(created);
 
+    }
+
+    //GET all authors
+    @GetMapping
+    public ResponseEntity<List<AuthorDTO>> getAllAuthors() {
+
+      //  List<AuthorDTO> getAll = authorService.getAllAuthors();
+        return ResponseEntity.ok(authorService.getAllAuthors());
     }
 
 
