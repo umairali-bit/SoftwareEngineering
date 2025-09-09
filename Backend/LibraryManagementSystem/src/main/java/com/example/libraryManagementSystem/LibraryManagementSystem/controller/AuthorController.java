9a0aeaf81f65c9d37a3ad52e17a95ed6678fc8fb
Umair Ali <umairmamoor@gmail.com>
2025-09-09 01:27:02 -0400

    createAuthor controller

--- Backend/LibraryManagementSystem/src/main/java/com/example/libraryManagementSystem/LibraryManagementSystem/controller/AuthorController.java
+++ Backend/LibraryManagementSystem/src/main/java/com/example/libraryManagementSystem/LibraryManagementSystem/controller/AuthorController.java
@@ -1,8 +1,13 @@
 package com.example.libraryManagementSystem.LibraryManagementSystem.controller;
 
 
+import com.example.libraryManagementSystem.LibraryManagementSystem.dtos.AuthorDTO;
 import com.example.libraryManagementSystem.LibraryManagementSystem.services.AuthorService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +17,17 @@ import org.springframework.web.bind.annotation.RestController;
 public class AuthorController {
 
     private final AuthorService authorService;
+
+
+    //create a new AUTHOR
+    @PostMapping
+    public ResponseEntity<AuthorDTO> createAuthor(@RequestBody AuthorDTO authorDTO) {
+
+        AuthorDTO created = authorService.createAuthor(authorDTO);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(created);
+
+    }
 
 
 }
