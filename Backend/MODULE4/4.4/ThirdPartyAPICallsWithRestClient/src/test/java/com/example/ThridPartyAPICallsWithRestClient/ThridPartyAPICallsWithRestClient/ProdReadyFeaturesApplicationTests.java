56641bb503a127b9156d42d84bab5fe52e25151e
Umair Ali <umairmamoor@gmail.com>
2025-09-18 03:31:11 -0400

    Orders of the tests

--- Backend/MODULE4/4.4/ThirdPartyAPICallsWithRestClient/src/test/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/ProdReadyFeaturesApplicationTests.java
+++ Backend/MODULE4/4.4/ThirdPartyAPICallsWithRestClient/src/test/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/ProdReadyFeaturesApplicationTests.java
@@ -2,8 +2,10 @@ package com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestC
 
 import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.clients.EmployeeClient;
 import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.dtos.EmployeeDTO;
-import lombok.Data;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureGraphQlTester;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -12,18 +14,21 @@ import java.util.Arrays;
 import java.util.List;
 
 @SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
 class ProdReadyFeaturesApplicationTests {
 
 	@Autowired
 	private EmployeeClient employeeClient;
 
 	@Test
+   @Order(1)
 	void getAllEmployees() {
 		List<EmployeeDTO> employeeDTOList = employeeClient.getAllEmployees();
 		System.out.println(employeeDTOList);
 	}
 
 	@Test
+    @org.junit.jupiter.api.Order(2)
 	void getEmployeeById() {
 		EmployeeDTO employeeDTO = employeeClient.getEmployeeById(1L); {
 			System.out.println(employeeDTO);
@@ -32,6 +37,7 @@ class ProdReadyFeaturesApplicationTests {
 	}
 
     @Test
+    @org.junit.jupiter.api.Order(1)
     void createEmployee() {
         EmployeeDTO employeeDTO = new EmployeeDTO();
         employeeDTO.setName("Saul");
