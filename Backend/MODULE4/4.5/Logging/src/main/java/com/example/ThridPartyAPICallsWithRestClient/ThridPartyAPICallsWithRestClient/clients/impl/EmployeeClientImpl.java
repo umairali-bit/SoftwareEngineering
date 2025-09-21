f310c128d51806c6f01d91a556de320ebc480aa0
Umair Ali <umairmamoor@gmail.com>
2025-09-20 23:55:29 -0400

    logging

---
+++ Backend/MODULE4/4.5/Logging/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/clients/impl/EmployeeClientImpl.java
@@ -0,0 +1,74 @@
+package com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.clients.impl;
+
+import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.advices.ApiResponse;
+import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.clients.EmployeeClient;
+import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.dtos.EmployeeDTO;
+import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.exceptions.ResourceNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class EmployeeClientImpl implements EmployeeClient {
+
+    private final RestClient restClient;
+
+    @Override
+    public List<EmployeeDTO> getAllEmployees() {
+
+        try{
+            ApiResponse<List<EmployeeDTO>> employeeDTO = restClient.get()
+                    .uri("employees")
+                    .retrieve()
+                    .body(new ParameterizedTypeReference<>() {
+                    });
+
+            return employeeDTO.getData();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public EmployeeDTO getEmployeeById(Long employeeId) {
+        try{
+            ApiResponse<EmployeeDTO> employeeDTOApiResponse = restClient.get()
+                    .uri("employees/{employeeId}", employeeId)
+                    .retrieve()
+                    .body(new ParameterizedTypeReference<ApiResponse<EmployeeDTO>>() {
+                    });
+            return employeeDTOApiResponse.getData();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public EmployeeDTO createEmployee(EmployeeDTO employeeDTO) {
+        try{
+            ResponseEntity<ApiResponse<EmployeeDTO>> employeeDTOApiResponse = restClient.post()
+                    .uri("employees")
+                    .body(employeeDTO)
+                    .retrieve()
+                    .onStatus(HttpStatusCode::is4xxClientError, ((request, response) -> {
+                        //print all the errors - not recommended using sout
+                        System.out.println(new String(response.getBody().readAllBytes()));
+                        throw new ResourceNotFoundException("Employee could not be created");
+                    }))
+                    .toEntity(new ParameterizedTypeReference<>() {
+                    });
+            return employeeDTOApiResponse.getBody().getData();
+
+        }
+        catch (Exception e){
+            throw new RuntimeException(e);
+        }
+    }
+}
