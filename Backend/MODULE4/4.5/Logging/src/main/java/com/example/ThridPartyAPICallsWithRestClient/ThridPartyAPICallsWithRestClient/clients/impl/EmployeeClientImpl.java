5bb4e8c097bb608a854045dbc8cac297a23a73f0
Umair Ali <umairmamoor@gmail.com>
2025-09-23 02:55:25 -0400

    logging

--- Backend/MODULE4/4.5/Logging/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/clients/impl/EmployeeClientImpl.java
+++ Backend/MODULE4/4.5/Logging/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/clients/impl/EmployeeClientImpl.java
@@ -5,6 +5,8 @@ import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestCl
 import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.dtos.EmployeeDTO;
 import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.exceptions.ResourceNotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.core.ParameterizedTypeReference;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
@@ -19,8 +21,16 @@ public class EmployeeClientImpl implements EmployeeClient {
 
     private final RestClient restClient;
 
+    Logger log = LoggerFactory.getLogger(EmployeeClientImpl.class);
+
     @Override
     public List<EmployeeDTO> getAllEmployees() {
+
+        log.error("error log");
+        log.info("info log");
+        log.warn("warn log");
+        log.debug("debug log");
+        log.trace("trace log");
 
         try{
             ApiResponse<List<EmployeeDTO>> employeeDTO = restClient.get()
