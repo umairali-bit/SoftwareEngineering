b994db862fc5a328620d76d50545fe167e84c6a5
Umair Ali <umairmamoor@gmail.com>
2025-10-01 02:57:07 -0400

    edit configurations

--- Backend/currency-converter/src/main/java/com/example/currencyconverter/services/CurrencyService.java
+++ Backend/currency-converter/src/main/java/com/example/currencyconverter/services/CurrencyService.java
@@ -1,12 +1,29 @@
 package com.example.currencyconverter.services;
 
-import lombok.Value;
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 
 @Service
 public class CurrencyService {
 
+    @Value("${freecurrency.api.base-url}")
+    private String baseUrl;
+
+    @Value("${freecurrency.api.key}")
+    private String apiKey;
+
+    private final RestClient currencyRestClient;
+
+    public CurrencyService(RestClient currencyRestClient) {
+        this.currencyRestClient = currencyRestClient;
+    }
 
 
-
+    @PostConstruct
+    public void testConfig() {
+        System.out.println("Base URL: " + baseUrl);
+        System.out.println("API Key Loaded: " + apiKey.substring(0,6) + "*****");
+    }
 }
