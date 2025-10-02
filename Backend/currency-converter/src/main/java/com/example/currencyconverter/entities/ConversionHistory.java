790119c862a99955f4c14c9b8324a596246d1928
Umair Ali <umairmamoor@gmail.com>
2025-10-02 04:28:57 -0400

    entity - conversion history

---
+++ Backend/currency-converter/src/main/java/com/example/currencyconverter/entities/ConversionHistory.java
@@ -0,0 +1,39 @@
+package com.example.currencyconverter.entities;
+
+
+import jakarta.persistence.*;
+import lombok.Generated;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
+
+
+@Getter
+@Setter
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Table(name = "conversion_history")
+
+public class ConversionHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fromCurrency;
+    private String toCurrency;
+    private double units;
+    private double rates;
+    private double convertedAmount;
+
+    @CreatedDate
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate
+    private LocalDateTime lastModifiedDate;
+}
