43135ee8fa11c478d95dfd4efa87878666a2df1f
Umair Ali <umairmamoor@gmail.com>
2025-10-19 15:34:50 -0400

    Hotel Browser controller

---
+++ Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/controllers/HotelBrowseController.java
@@ -0,0 +1,16 @@
+package com.nestigo.systemdesign.nestigo.controllers;
+
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hotels")
+@RequiredArgsConstructor
+@Slf4j
+public class HotelBrowseController {
+
+
+}
