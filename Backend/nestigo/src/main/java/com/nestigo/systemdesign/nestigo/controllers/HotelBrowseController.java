9c7306c849b8f195ccd56d508d5bbd94c19e27c5
Umair Ali <umairmamoor@gmail.com>
2025-10-19 15:38:27 -0400

    HotelSearchRequest

--- Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/controllers/HotelBrowseController.java
+++ Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/controllers/HotelBrowseController.java
@@ -3,6 +3,7 @@ package com.nestigo.systemdesign.nestigo.controllers;
 
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @Slf4j
 public class HotelBrowseController {
+
+    @GetMapping("/search")
+
 
 
 }
