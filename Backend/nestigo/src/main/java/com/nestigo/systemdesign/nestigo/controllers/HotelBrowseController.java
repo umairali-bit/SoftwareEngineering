e2a8abf88e97b7d5710a4e104d87e0fbd8c07ae3
Umair Ali <umairmamoor@gmail.com>
2025-11-18 02:54:03 -0500

    HotelInfoDTO plus getMapping{hotelId}

--- Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/controllers/HotelBrowseController.java
+++ Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/controllers/HotelBrowseController.java
@@ -2,7 +2,9 @@ package com.nestigo.systemdesign.nestigo.controllers;
 
 
 import com.nestigo.systemdesign.nestigo.dtos.HotelDTO;
+import com.nestigo.systemdesign.nestigo.dtos.HotelInfoDTO;
 import com.nestigo.systemdesign.nestigo.dtos.HotelSearchRequest;
+import com.nestigo.systemdesign.nestigo.services.HotelService;
 import com.nestigo.systemdesign.nestigo.services.InventoryService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,12 +21,20 @@ import java.util.List;
 public class HotelBrowseController {
 
     private final InventoryService inventoryService;
+    private final HotelService hotelService;
 
     @GetMapping("/search")
     public ResponseEntity<Page<HotelDTO>> searchHotels(@RequestBody HotelSearchRequest hotelSearchRequest) {
         Page<HotelDTO> page = inventoryService.searchHotels(hotelSearchRequest);
         return ResponseEntity.ok(page);
     }
+
+    @GetMapping("/{hotelId}")
+    public ResponseEntity<HotelInfoDTO> getHotelInfo(@PathVariable("hotelId") String hotelId) {
+        return ResponseEntity.ok(hotelService.getHotelInfoById(hotelId));
+    }
+
+
 
 
 
---
+++ Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/dtos/HotelInfoDTO.java
@@ -0,0 +1,10 @@
+package com.nestigo.systemdesign.nestigo.dtos;
+
+import java.util.List;
+
+public class HotelInfoDTO {
+
+    private HotelDTO hotel;
+    private List<RoomDTO> rooms;
+
+}
