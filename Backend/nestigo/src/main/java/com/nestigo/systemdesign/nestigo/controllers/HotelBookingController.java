f9c4b12b21faf6b76ddb411890b189b75f00ebcf
Umair Ali <umairmamoor@gmail.com>
2025-11-19 07:25:11 -0500

    BookingRequest

--- Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/controllers/HotelBookingController.java
+++ Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/controllers/HotelBookingController.java
@@ -1,7 +1,9 @@
 package com.nestigo.systemdesign.nestigo.controllers;
 
 
+import com.nestigo.systemdesign.nestigo.dtos.BookingDTO;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,6 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 @RequestMapping("/bookings")
 public class HotelBookingController {
+
+    public ResponseEntity<BookingDTO> initializeBookings(BookingDTO bookingDTO) {
+        return ResponseEntity.ok(bookingDTO);
+    }
 
 
 
