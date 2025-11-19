5418dc9b096dbaaf71b90d317627852ac07ce9ae
Umair Ali <umairmamoor@gmail.com>
2025-11-19 07:11:26 -0500

    BookingDTO fields

--- Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/entities/enums/BookingStatus.java
+++ Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/entities/enums/BookingStatus.java
@@ -3,6 +3,9 @@ package com.nestigo.systemdesign.nestigo.entities.enums;
 public enum BookingStatus {
 
     RESERVED,
+    GUEST_ADDED,
+    PAYMENT_PENDING,
     CONFIRMED,
-    CANCELLED
+    CANCELLED,
+    EXPIRED
 }
