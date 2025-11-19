af1d1d5655f5f5bdae82bc1aa6d482c3b3a44ec7
Umair Ali <umairmamoor@gmail.com>
2025-11-19 06:52:38 -0500

    HotelBookingController

---
+++ Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/controllers/HotelBookingController.java
@@ -0,0 +1,15 @@
+package com.nestigo.systemdesign.nestigo.controllers;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/bookings")
+public class HotelBookingController {
+
+
+
+}
