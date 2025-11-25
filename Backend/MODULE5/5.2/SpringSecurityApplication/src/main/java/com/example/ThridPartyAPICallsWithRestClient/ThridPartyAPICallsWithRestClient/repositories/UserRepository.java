7c6531b57962ef32fa3defc23219ddf089a5e1c6
Umair Ali <umairmamoor@gmail.com>
2025-11-25 03:25:46 -0500

    notes

--- Backend/MODULE5/5.2/SpringSecurityApplication/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/repositories/UserRepository.java
+++ Backend/MODULE5/5.2/SpringSecurityApplication/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/repositories/UserRepository.java
@@ -10,7 +10,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
 
-    Optional<UserEntity> findByEmail();
+    Optional<UserEntity> findByEmail(String email);
 
 
 }
