b30584664ecce9240a79433e1b44db06df7ebb37
Umair Ali <umairmamoor@gmail.com>
2025-11-25 02:38:27 -0500

    5.2

---
+++ Backend/MODULE5/5.2/SpringSecurityApplication/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/controllers/PostController.java
@@ -0,0 +1,38 @@
+package com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.controllers;
+
+
+import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.dtos.PostDTO;
+import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.services.PostService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "/posts")
+public class PostController {
+
+    private final PostService postService;
+
+    public PostController(PostService postService) {
+        this.postService = postService;
+    }
+
+    @GetMapping
+    public List<PostDTO> getAllPosts() {
+        return postService.getAllPosts();
+    }
+
+    @GetMapping("/{postId}")
+    public PostDTO getPostById(@PathVariable Long postId) {
+        return postService.getPostById(postId);
+    }
+
+    @PostMapping
+    public PostDTO createNewPost(@RequestBody PostDTO inputPost) {
+        return postService.createNewPost(inputPost);
+    }
+
+
+}
+
+
