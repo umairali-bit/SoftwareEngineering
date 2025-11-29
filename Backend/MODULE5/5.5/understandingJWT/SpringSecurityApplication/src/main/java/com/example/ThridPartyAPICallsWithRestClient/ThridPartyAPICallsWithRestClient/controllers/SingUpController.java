399366a97fab3a44d868c42eaad2a1199ffdb138
Umair Ali <umairmamoor@gmail.com>
2025-11-29 07:18:08 -0500

    singUp controller

--- Backend/MODULE5/5.5/understandingJWT/SpringSecurityApplication/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/controllers/SingUpController.java
+++ Backend/MODULE5/5.5/understandingJWT/SpringSecurityApplication/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/controllers/SingUpController.java
@@ -2,19 +2,32 @@ package com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestC
 
 
 import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.dtos.SignUpDTO;
+import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.dtos.UserDto;
+import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.services.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping(path = "/auth")
 
 public class SingUpController {
 
+    private final UserService userService;
+
+
+
     @PostMapping("/signUp")
-    public String signUp(@RequestBody SignUpDTO inputSignUp) {
+    public ResponseEntity<UserDto> signUp(@RequestBody @Valid SignUpDTO inputSignUp) {
+
+        UserDto userDto = userService.singUp(inputSignUp);
+        return ResponseEntity.ok(userDto);
+
 
     }
 
