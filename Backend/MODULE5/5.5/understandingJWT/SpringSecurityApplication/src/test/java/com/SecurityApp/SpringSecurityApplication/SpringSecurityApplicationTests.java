7df58eee69eede2c6a133244312329dc6911b003
Umair Ali <umairmamoor@gmail.com>
2025-11-29 01:50:16 -0500

    testing

--- Backend/MODULE5/5.5/understandingJWT/SpringSecurityApplication/src/test/java/com/SecurityApp/SpringSecurityApplication/SpringSecurityApplicationTests.java
+++ Backend/MODULE5/5.5/understandingJWT/SpringSecurityApplication/src/test/java/com/SecurityApp/SpringSecurityApplication/SpringSecurityApplicationTests.java
@@ -1,13 +1,32 @@
 package com.SecurityApp.SpringSecurityApplication;
 
+import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.ProdReadyFeaturesApplication;
+import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.entities.UserEntity;
+import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.services.JwtService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.core.userdetails.User;
 
-@SpringBootTest
+@SpringBootTest(classes = ProdReadyFeaturesApplication.class)
 class SpringSecurityApplicationTests {
+
+
+    @Autowired
+    private JwtService jwtService;
 
 	@Test
 	void contextLoads() {
+
+
+        UserEntity user = new UserEntity(4L, "janeDoe@gmail.com", "1234");
+
+        String token = jwtService.generateJwtToken(user);
+        System.out.println(token);
+
+        Long id = jwtService.getUserIdFromJwtToken(token);
+        System.out.println(id);
+
 	}
 
 }
