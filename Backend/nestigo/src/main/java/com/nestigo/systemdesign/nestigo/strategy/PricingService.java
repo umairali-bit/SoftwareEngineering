13797c2cac70bfd69246f74af4d93db7e354ddeb
Umair Ali <umairmamoor@gmail.com>
2025-12-13 04:16:43 -0500

    PricingService

---
+++ Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/strategy/PricingService.java
@@ -0,0 +1,24 @@
+package com.nestigo.systemdesign.nestigo.strategy;
+
+
+import com.nestigo.systemdesign.nestigo.entities.InventoryEntity;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class PricingService {
+
+    public BigDecimal calculateDynamicPricing(InventoryEntity inventory) {
+        PricingStrategy pricingStrategy = new BasePriceStrategy();
+
+        // apply the additional strategies
+        pricingStrategy = new SurgePricingStrategy(pricingStrategy);
+        pricingStrategy = new OccupancyPricingStrategy(pricingStrategy);
+        pricingStrategy = new UrgencyPricingStrategy(pricingStrategy);
+        pricingStrategy = new HolidayPricingStrategy(pricingStrategy);
+
+        return pricingStrategy.calculatePrice(inventory);
+
+    }
+}
