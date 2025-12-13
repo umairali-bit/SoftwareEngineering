5e0841160d9dafcc0bd780cc8e07cfa34119e258
Umair Ali <umairmamoor@gmail.com>
2025-12-13 03:02:53 -0500

    pricing stratedy interface

---
+++ Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/strategy/PricingStrategy.java
@@ -0,0 +1,12 @@
+package com.nestigo.systemdesign.nestigo.strategy;
+
+import com.nestigo.systemdesign.nestigo.entities.InventoryEntity;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public interface PricingStrategy {
+
+    BigDecimal calculatePrice(InventoryEntity inventory);
+
+}
