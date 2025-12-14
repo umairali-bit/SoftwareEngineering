dd663e74f2f6c7a87013e5856e7d1d2a16d5d56f
Umair Ali <umairmamoor@gmail.com>
2025-12-14 04:39:06 -0500

    hotelMinPrice Repository

---
+++ Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/repositories/HotelMinPrice.java
@@ -0,0 +1,34 @@
+package com.nestigo.systemdesign.nestigo.repositories;
+
+import com.nestigo.systemdesign.nestigo.entities.HotelEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.time.LocalDate;
+
+@Repository
+public interface HotelMinPrice extends JpaRepository<HotelMinPrice, Integer> {
+
+    @Query("""
+    SELECT DISTINCT i.hotel
+    FROM InventoryEntity i
+    WHERE i.city = :city
+      AND i.date BETWEEN :startDate AND :endDate
+      AND i.closed = false
+      AND (i.totalCount - i.bookedCount - i.reservedCount) >= :roomsCount
+    GROUP BY i.hotel, i.room
+    HAVING COUNT(i.date) = :dateCount
+""")
+    Page<HotelEntity> findHotelsWithAvailableInventory(
+            @Param("city") String city,
+            @Param("startDate") LocalDate startDate,
+            @Param("endDate") LocalDate endDate,
+            @Param("roomsCount") Integer roomCount,
+            @Param("dateCount") Long dateCount,
+            Pageable pageable
+    );
+}
