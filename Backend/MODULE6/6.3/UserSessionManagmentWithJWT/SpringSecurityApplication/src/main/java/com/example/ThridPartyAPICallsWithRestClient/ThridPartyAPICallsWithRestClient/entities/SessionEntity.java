1edfab2184a4d84206b5acc319e043fae2db02bd
Umair Ali <umairmamoor@gmail.com>
2025-12-23 02:48:00 -0500

    Session Entity

---
+++ Backend/MODULE6/6.3/UserSessionManagmentWithJWT/SpringSecurityApplication/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/entities/SessionEntity.java
@@ -0,0 +1,25 @@
+package com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.entities;
+
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class SessionEntity {
+
+@Id
+@GeneratedValue(strategy= GenerationType.IDENTITY)
+private Long  id;
+
+
+private String refreshToken;
+
+@CreationTimestamp
+private LocalDateTime lastUsedAt;
+
+@ManyToOne
+private UserEntity user;
+
+}
---
+++ Backend/MODULE6/6.3/UserSessionManagmentWithJWT/SpringSecurityApplication/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/services/SessionService.java
@@ -0,0 +1,10 @@
+package com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.services;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class SessionService {
+
+
+}
