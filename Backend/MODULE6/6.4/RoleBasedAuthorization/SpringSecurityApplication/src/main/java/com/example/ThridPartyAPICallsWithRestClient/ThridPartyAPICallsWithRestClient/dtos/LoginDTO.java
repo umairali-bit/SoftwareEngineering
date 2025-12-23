e11d3a4f7eca9dfdad49ed2ccfa7a68b5c95151c
Umair Ali <umairmamoor@gmail.com>
2025-12-23 05:54:17 -0500

    role based authorization

---
+++ Backend/MODULE6/6.4/RoleBasedAuthorization/SpringSecurityApplication/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/dtos/LoginDTO.java
@@ -0,0 +1,11 @@
+package com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.dtos;
+
+
+import lombok.Data;
+
+@Data
+public class LoginDTO {
+
+    private String email;
+    private String password;
+}
---
+++ Backend/MODULE6/6.4/RoleBasedAuthorization/SpringSecurityApplication/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/dtos/SignUpDTO.java
@@ -0,0 +1,21 @@
+package com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.dtos;
+
+
+import lombok.Data;
+import jakarta.validation.constraints.*;
+import org.aspectj.bridge.IMessage;
+
+@Data
+public class SignUpDTO {
+
+    @NotBlank(message = "Name is required")
+    private String name;
+
+    @Email(message = "Invalid email format")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, message = "Password must be at least 8 characters")
+    private String password;
+}
