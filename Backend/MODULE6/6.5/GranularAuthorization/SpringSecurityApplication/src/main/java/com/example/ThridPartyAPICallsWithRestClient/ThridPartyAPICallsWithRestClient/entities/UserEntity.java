ac47ad20c77d6044750410531341c2198d612f72
Umair Ali <umairmamoor@gmail.com>
2025-12-24 02:56:33 -0500

    Adding permissions to Spring Security

--- Backend/MODULE6/6.5/GranularAuthorization/SpringSecurityApplication/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/entities/UserEntity.java
+++ Backend/MODULE6/6.5/GranularAuthorization/SpringSecurityApplication/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/entities/UserEntity.java
@@ -1,6 +1,7 @@
 package com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.entities;
 
 
+import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.entities.enums.Permissions;
 import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.entities.enums.Role;
 import jakarta.persistence.*;
 import lombok.*;
@@ -37,13 +38,24 @@ public class UserEntity implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Enumerated(EnumType.STRING)
+    private Set<Permissions> permissions;
+
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return roles.stream()
+        Set<SimpleGrantedAuthority> authorities = roles.stream()
                 .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))
                 .collect(Collectors.toSet());
+
+        permissions.forEach(
+                permission -> authorities.add(new SimpleGrantedAuthority(permission.name())));
+
+
+        return authorities;
     }
+
 
     @Override
     public String getPassword() {
