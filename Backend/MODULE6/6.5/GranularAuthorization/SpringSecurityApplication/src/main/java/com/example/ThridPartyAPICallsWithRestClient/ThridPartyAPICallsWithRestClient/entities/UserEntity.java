c82f9d4e519d607b455d75f9a4e0c5d237e2603a
Umair Ali <umairmamoor@gmail.com>
2025-12-25 07:20:36 -0500

    removing permissions in DB

--- Backend/MODULE6/6.5/GranularAuthorization/SpringSecurityApplication/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/entities/UserEntity.java
+++ Backend/MODULE6/6.5/GranularAuthorization/SpringSecurityApplication/src/main/java/com/example/ThridPartyAPICallsWithRestClient/ThridPartyAPICallsWithRestClient/entities/UserEntity.java
@@ -5,6 +5,7 @@ import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestCl
 import com.example.ThridPartyAPICallsWithRestClient.ThridPartyAPICallsWithRestClient.entities.enums.Role;
 import jakarta.persistence.*;
 import lombok.*;
+import org.hibernate.grammars.hql.HqlParser;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -38,9 +39,10 @@ public class UserEntity implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Set<Role> roles;
 
-    @ElementCollection(fetch = FetchType.EAGER)
-    @Enumerated(EnumType.STRING)
-    private Set<Permissions> permissions;
+//      Having permission in database
+//    @ElementCollection(fetch = FetchType.EAGER)
+//    @Enumerated(EnumType.STRING)
+//    private Set<Permissions> permissions;
 
 
     @Override
@@ -49,8 +51,8 @@ public class UserEntity implements UserDetails {
                 .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))
                 .collect(Collectors.toSet());
 
-        permissions.forEach(
-                permission -> authorities.add(new SimpleGrantedAuthority(permission.name())));
+//        permissions.forEach(
+//                permission -> authorities.add(new SimpleGrantedAuthority(permission.name()))); permissions in DB
 
 
         return authorities;
