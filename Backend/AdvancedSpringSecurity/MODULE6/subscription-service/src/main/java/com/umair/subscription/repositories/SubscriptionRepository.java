6d1c98f0c0fc4fd62a390adef4aabf4d06c473c9
Umair Ali <umairmamoor@gmail.com>
2025-12-28 15:35:31 -0500

    query to find the current entitlement

--- Backend/AdvancedSpringSecurity/MODULE6/subscription-service/src/main/java/com/umair/subscription/repositories/SubscriptionRepository.java
+++ Backend/AdvancedSpringSecurity/MODULE6/subscription-service/src/main/java/com/umair/subscription/repositories/SubscriptionRepository.java
@@ -4,8 +4,26 @@ package com.umair.subscription.repositories;
 import com.umair.subscription.entities.SubscriptionEntity;
 import org.springframework.boot.autoconfigure.pulsar.PulsarProperties;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface SubscriptionRepository extends JpaRepository<SubscriptionEntity, Integer> {
+
+    @Query("""
+         select s from SubscriptionEntity s\s
+         where s.user.id = :userId
+         and s.subscriptionStatus in (
+            com.umair.subscription.entities.enums.SubscriptionStatus.ACTIVE,
+            com.umair.subscription.entities.enums.SubscriptionStatus.CANCELED
+            )
+           \s
+            and s.startAt <= now
+            and (s.endAt is null or s.endAt > : now)
+            order by s.startAt desc
+           \s""")
+    Optional <SubscriptionEntity> findCurrentEntitlement(Integer userId);
 }
