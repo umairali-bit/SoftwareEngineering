fa36d6ddaac7c11824d929e0a856ffff52e12112
Umair Ali <umairmamoor@gmail.com>
2025-12-29 06:38:02 -0500

    CustomUserDetailService

--- Backend/AdvancedSpringSecurity/MODULE6/subscription-service/src/main/java/com/umair/subscription/repositories/UserRepository.java
+++ Backend/AdvancedSpringSecurity/MODULE6/subscription-service/src/main/java/com/umair/subscription/repositories/UserRepository.java
@@ -4,7 +4,11 @@ import com.umair.subscription.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<UserEntity, Long> {
     boolean existsByEmail(String email);
+
+   Optional<UserEntity>  findByEmail(String email);
 }
---
+++ Backend/AdvancedSpringSecurity/MODULE6/subscription-service/src/main/java/com/umair/subscription/services/CustomUserDetailsService.java
@@ -0,0 +1,33 @@
+package com.umair.subscription.services;
+
+
+import com.umair.subscription.entities.UserEntity;
+import com.umair.subscription.repositories.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+
+
+    @Override
+    public UserDetails loadUserByUsername(String email){
+
+        UserEntity user = userRepository.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found " + email));
+
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())   // MUST be hashed
+                .authorities("ROLE_USER")
+                .build();
+    }
+}
