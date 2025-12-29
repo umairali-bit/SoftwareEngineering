f95173130496b1e4212d0357c96aad9af0a0dde1
Umair Ali <umairmamoor@gmail.com>
2025-12-29 06:21:08 -0500

    Security config

---
+++ Backend/AdvancedSpringSecurity/MODULE6/subscription-service/src/main/java/com/umair/subscription/config/SecurityConfig.java
@@ -0,0 +1,32 @@
+package com.umair.subscription.config;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(securedEnabled = true)
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth-> auth
+                        .requestMatchers("/signup", "/login", "/oauth2/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                .oauth2Login(oauth ->{})
+    .build();
+    }
+}
+
+
+
