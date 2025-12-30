b68dd2aa10a01b952fef1280ff639c65332b33e9
Umair Ali <umairmamoor@gmail.com>
2025-12-30 03:16:58 -0500

    logout of all devices service controller

--- Backend/AdvancedSpringSecurity/MODULE6/subscription-service/src/main/java/com/umair/subscription/controllers/AuthController.java
+++ Backend/AdvancedSpringSecurity/MODULE6/subscription-service/src/main/java/com/umair/subscription/controllers/AuthController.java
@@ -2,12 +2,14 @@ package com.umair.subscription.controllers;
 
 
 import com.umair.subscription.dto.*;
+import com.umair.subscription.entities.UserEntity;
 import com.umair.subscription.services.AuthService;
 import com.umair.subscription.services.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.apache.coyote.Response;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -45,6 +47,14 @@ public class AuthController {
     public ResponseEntity<?> logout(@RequestBody LogoutRequestDTO request) {
         authService.logout(request);
         return ResponseEntity.ok(Map.of("message", "Logged out successfully"));
+    }
+
+    @PostMapping("/logout-all")
+    public ResponseEntity<?> logoutAll(Authentication authentication) {
+        UserEntity user = (UserEntity) authentication.getPrincipal();
+        int count = authService.logoutAllDevices(user.getId());
+
+        return ResponseEntity.ok(Map.of("message", "Logged out from all devices", "revoked", String.valueOf(count)));
     }
 
 }
