c1c2b99e0dbc519c0ad8b677582e771a768eda18
Umair Ali <umairmamoor@gmail.com>
2025-12-30 03:11:13 -0500

    logout of all devices repository methods

--- Backend/AdvancedSpringSecurity/MODULE6/subscription-service/src/main/java/com/umair/subscription/repositories/SessionRepository.java
+++ Backend/AdvancedSpringSecurity/MODULE6/subscription-service/src/main/java/com/umair/subscription/repositories/SessionRepository.java
@@ -3,6 +3,8 @@ package com.umair.subscription.repositories;
 import com.umair.subscription.entities.SessionEntity;
 import com.umair.subscription.entities.enums.SessionStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -11,4 +13,15 @@ import java.util.Optional;
 public interface SessionRepository extends JpaRepository<SessionEntity,Long> {
 
     Optional<SessionEntity> findByRefreshTokenHashAndStatus(String refreshTokenHash, SessionStatus sessionStatus);
+
+    @Modifying
+    @Query("""
+        update SessionEntity s
+        set s.status = com.umair.subscription.entities.enums.SessionStatus.REVOKED
+        where s.user.id = :userId
+            and s.status = com.umair.subscription.entities.enums.SessionStatus.ACTIVE
+""")
+    int revokeAllActiveSessions(Long userId);
+
+
 }
