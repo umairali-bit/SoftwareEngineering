cbec9b06bc7306e1fd1e6d9c7dc040288dbd546e
Umair Ali <umairmamoor@gmail.com>
2025-12-31 06:05:31 -0500

    JwtFilter

---
+++ Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/security/JwtAuthFilter.java
@@ -0,0 +1,57 @@
+package com.nestigo.systemdesign.nestigo.security;
+
+import com.nestigo.systemdesign.nestigo.entities.UserEntity;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+
+@Component
+@RequiredArgsConstructor
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtService jwtService;
+
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        final String authorizationHeader = request.getHeader("Authorization");
+        if (authorizationHeader != null && authorizationHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        String token = authorizationHeader.split("Bearer ")[1];
+        Long userId = jwtService.getUserIdFromJwtToken(token);
+
+        if (userId == null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserEntity user = userService.getUserById(userId);
+            UsernamePasswordAuthenticationToken authentication =
+                    new UsernamePasswordAuthenticationToken(user, null, user.getAuthorities());
+            authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+
+
+
+
+
+    }
+}
