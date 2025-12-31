bef9c3586cc537ca085a55d861dbfa8876b922fe
Umair Ali <umairmamoor@gmail.com>
2025-12-31 02:10:06 -0500

    UserEntity implements UserDetails Interface

--- Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/entities/UserEntity.java
+++ Backend/nestigo/src/main/java/com/nestigo/systemdesign/nestigo/entities/UserEntity.java
@@ -5,14 +5,20 @@ import com.nestigo.systemdesign.nestigo.entities.enums.RoleEnum;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Entity
 @Getter
 @Setter
 @Table(name = "app_user")//name cannot be user if we are using postgres
-public class UserEntity {
+public class UserEntity implements UserDetails {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -31,8 +37,15 @@ public class UserEntity {
     private Set<RoleEnum> roles;
 
 
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority("ROLE_" + role.name()))
+                .collect(Collectors.toSet());
+    }
 
-
-
-
+    @Override
+    public String getUsername() {
+        return email;
+    }
 }
