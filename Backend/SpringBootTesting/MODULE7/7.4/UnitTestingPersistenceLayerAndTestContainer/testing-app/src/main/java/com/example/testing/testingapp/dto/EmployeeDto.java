2856506a1cbcd631af23285cdfc1c5b761447833
Umair Ali <umairmamoor@gmail.com>
2026-01-03 03:22:48 -0500

    service interface

--- Backend/SpringBootTesting/MODULE7/7.4/UnitTestingPersistenceLayerAndTestContainer/testing-app/src/main/java/com/example/testing/testingapp/dto/EmployeeDto.java
+++ Backend/SpringBootTesting/MODULE7/7.4/UnitTestingPersistenceLayerAndTestContainer/testing-app/src/main/java/com/example/testing/testingapp/dto/EmployeeDto.java
@@ -10,7 +10,7 @@ import lombok.NoArgsConstructor;
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class Employeedto {
+public class EmployeeDto {
 
     private Long id;
     private String name;
