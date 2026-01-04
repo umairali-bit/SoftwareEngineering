c859ad271126a6efc8dca92a4440fa015515a2df
Umair Ali <umairmamoor@gmail.com>
2026-01-04 09:48:39 -0500

    creating a persistence level test file

---
+++ Backend/SpringBootTesting/MODULE7/7.4/UnitTestingPersistenceLayerAndTestContainer/testing-app/src/test/java/com/example/testing/testingapp/repositories/EmployeeRepositoryTest.java
@@ -0,0 +1,12 @@
+package com.example.testing.testingapp.repositories;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EmployeeRepositoryTest {
+
+    @Test
+    void findByEmail() {
+    }
+}
