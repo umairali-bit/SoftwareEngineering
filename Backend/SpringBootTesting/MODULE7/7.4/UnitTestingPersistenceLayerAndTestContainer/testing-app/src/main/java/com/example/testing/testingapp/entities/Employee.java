a17560db2186c48d219753cf0c298b9f9b1e84b2
Umair Ali <umairmamoor@gmail.com>
2026-01-04 10:32:10 -0500

    test

--- Backend/SpringBootTesting/MODULE7/7.4/UnitTestingPersistenceLayerAndTestContainer/testing-app/src/main/java/com/example/testing/testingapp/entities/Employee.java
+++ Backend/SpringBootTesting/MODULE7/7.4/UnitTestingPersistenceLayerAndTestContainer/testing-app/src/main/java/com/example/testing/testingapp/entities/Employee.java
@@ -2,13 +2,19 @@ package com.example.testing.testingapp.entities;
 
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.modelmapper.internal.bytebuddy.dynamic.loading.InjectionClassLoader;
 
 import javax.annotation.processing.Generated;
 
 @Entity
 @Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Employee {
 
     @Id
--- Backend/SpringBootTesting/MODULE7/7.4/UnitTestingPersistenceLayerAndTestContainer/testing-app/src/test/java/com/example/testing/testingapp/repositories/EmployeeRepositoryTest.java
+++ Backend/SpringBootTesting/MODULE7/7.4/UnitTestingPersistenceLayerAndTestContainer/testing-app/src/test/java/com/example/testing/testingapp/repositories/EmployeeRepositoryTest.java
@@ -1,23 +1,55 @@
 package com.example.testing.testingapp.repositories;
 
+import com.example.testing.testingapp.entities.Employee;
+
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 
-@SpringBootTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
+
+@DataJpaTest
+// @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class EmployeeRepositoryTest {
 
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    private Employee employee;
+
+    @BeforeEach
+    void setUp() {
+        employee = Employee.builder()
+                .id(1L)
+                .name("Walter White")
+                .email("ww@example.com")
+                .salary(80_000L)
+                .build();
+    }
+
 
     @Test
-    void testFindByEmail_whenEmailIsValid_thenReturnEmployee() {
+    void testFindByEmail_whenEmailIsPresent_thenReturnEmployee() {
+ // Arrange
+
+    employeeRepository.save(employee);
+
+
+
+ // Act
+        List<Employee> employeeList = employeeRepository.findByEmail(employee.getEmail());
+
+
+ // Assert
+        assertThat(employeeList).isNotNull();
+        assertThat(employeeList).isNotEmpty();
 
 
     }
