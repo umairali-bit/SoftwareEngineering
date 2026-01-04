7016365461edae9205d35d365554609b84ba4a9e
Umair Ali <umairmamoor@gmail.com>
2026-01-04 10:11:38 -0500

    @DataJPATest

--- Backend/SpringBootTesting/MODULE7/7.4/UnitTestingPersistenceLayerAndTestContainer/testing-app/src/test/java/com/example/testing/testingapp/repositories/EmployeeRepositoryTest.java
+++ Backend/SpringBootTesting/MODULE7/7.4/UnitTestingPersistenceLayerAndTestContainer/testing-app/src/test/java/com/example/testing/testingapp/repositories/EmployeeRepositoryTest.java
@@ -2,12 +2,14 @@ package com.example.testing.testingapp.repositories;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 
 @SpringBootTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 class EmployeeRepositoryTest {
 
     @Autowired
