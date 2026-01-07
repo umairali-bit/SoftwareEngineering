021505c18c176cf2b1a2c8ce5b0255687603ccae
Umair Ali <umairmamoor@gmail.com>
2026-01-07 08:00:49 -0500

    Unit testing service layer using mockito

---
+++ Backend/SpringBootTesting/MODULE7/7.6/unitTestingServiceLayerUsingMockito/testing-app/src/main/java/com/example/testing/testingapp/services/EmployeeService.java
@@ -0,0 +1,12 @@
+package com.example.testing.testingapp.services;
+
+import com.example.testing.testingapp.dto.EmployeeDto;
+
+public interface EmployeeService {
+
+    EmployeeDto getEmployeeById(Long id);
+    EmployeeDto createNewEmployee(EmployeeDto employeeDto);
+    EmployeeDto updateEmployee(Long id, EmployeeDto employeeDto);
+    void deleteEmployee(Long id);
+
+}
---
+++ Backend/SpringBootTesting/MODULE7/7.6/unitTestingServiceLayerUsingMockito/testing-app/src/test/java/com/example/testing/testingapp/services/EmployeeServiceImplTest.java
@@ -0,0 +1,125 @@
+package com.example.testing.testingapp.services;
+
+import com.example.testing.testingapp.TestContainerConfiguration;
+import com.example.testing.testingapp.dto.EmployeeDto;
+import com.example.testing.testingapp.entities.Employee;
+import com.example.testing.testingapp.repositories.EmployeeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Spy;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.context.annotation.Import;
+import static org.mockito.ArgumentMatchers.any;
+import java.util.List;
+import java.util.Optional;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+
+@Import(TestContainerConfiguration.class)
+//@DataJpaTest (only for repositories)
+//@SpringBootTest (for integration testing)
+@ExtendWith(MockitoExtension.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class EmployeeServiceImplTest {
+
+    @Mock
+    private EmployeeRepository employeeRepository;
+
+    @InjectMocks
+    private EmployeeServiceImpl employeeService;
+
+    @Spy
+    private ModelMapper modelMapper;
+
+    private Employee mockEmployee;
+    private EmployeeDto mockEmployeeDto;
+
+    @BeforeEach
+    void setUp() {
+        mockEmployee = Employee.builder()
+                .id(1L)
+                .email("mockEmployee@xyz.com")
+                .name("mockEmployee")
+                .salary(80_000L)
+                .build();
+
+        mockEmployeeDto = EmployeeDto.builder()
+                .id(mockEmployee.getId())
+                .name(mockEmployee.getName())
+                .salary(mockEmployee.getSalary())
+                .email(mockEmployee.getEmail())
+                .build();
+
+
+
+        //or
+      //  mockEmployeeDto = modelMapper.map(mockEmployee, EmployeeDto.class);
+
+    }
+
+
+    @Test
+    void testGetEmployeeById_WhenEmployeeIdIsPresent_ThenReturnEmployeeDto() {
+
+//      assign
+        Long id = mockEmployee.getId();
+        when(employeeRepository.findById(id)).thenReturn(Optional.of(mockEmployee)); // stubbing
+
+//      act
+        EmployeeDto employeeDto = employeeService.getEmployeeById(1L);
+
+
+//      assert
+        assertThat(employeeDto).isNotNull();
+        assertThat(employeeDto.getId()).isEqualTo(id);
+        assertThat(employeeDto.getEmail()).isEqualTo(mockEmployee.getEmail());
+
+        verify(employeeRepository).findById(id);
+
+        /*
+        verify(employeeRepository, times(2)).findById(id);
+        verify(employeeRepository, atLeast(1)).findById(id);
+        verify(employeeRepository, atMost(1)).findById(id);
+        verify(employeeRepository, only()).findById(id);
+         */
+    }
+
+    @Test
+    void testCreateEmployee_IfValidEmployee_ThenCreateNewEmployee() {
+
+//         assign
+        when(employeeRepository.findByEmail(anyString())).thenReturn(List.of());
+        when(employeeRepository.save(any(Employee.class))).thenReturn(mockEmployee);
+
+
+//         act
+        EmployeeDto employeeDto = employeeService.createNewEmployee(mockEmployeeDto);
+
+//         assert
+        assertThat(employeeDto).isNotNull();
+        assertThat(employeeDto.getEmail()).isEqualTo(mockEmployeeDto.getEmail());
+
+        //verify(employeeRepository).save(any(Employee.class));
+
+        ArgumentCaptor<Employee> captor = ArgumentCaptor.forClass(Employee.class);
+        verify(employeeRepository).save(captor.capture());
+
+        Employee capturedEmployee = captor.getValue();
+        assertThat(capturedEmployee.getEmail()).isEqualTo(mockEmployee.getEmail());
+
+
+
+    }
+
+
+
+}
