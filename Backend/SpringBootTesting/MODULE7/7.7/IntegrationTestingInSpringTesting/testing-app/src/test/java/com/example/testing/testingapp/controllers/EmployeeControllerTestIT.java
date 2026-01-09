84b7ded0ae28e18e5739b88667bfb23d912ee1b4
Umair Ali <umairmamoor@gmail.com>
2026-01-09 02:18:00 -0500

    testing the EmployeeControllerTestIT

---
+++ Backend/SpringBootTesting/MODULE7/7.7/IntegrationTestingInSpringTesting/testing-app/src/test/java/com/example/testing/testingapp/controllers/EmployeeControllerTestIT.java
@@ -0,0 +1,27 @@
+package com.example.testing.testingapp.controllers;
+
+import com.example.testing.testingapp.TestContainerConfiguration;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+
+@AutoConfigureWebTestClient
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Import(TestContainerConfiguration.class)
+class EmployeeControllerTestIT {
+
+    @Autowired
+    private WebTestClient webTestClient;
+
+    @Test
+    void test() {
+
+    }
+
+
+
+}
