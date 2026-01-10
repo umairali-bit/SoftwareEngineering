c843f269b65083b859738e15c2da58618fb93f71
Umair Ali <umairmamoor@gmail.com>
2026-01-10 02:12:46 -0500

    TestContainerConfiguration

---
+++ Backend/SpringBootTesting/MODULE7/project/LibraryManagementSystem/src/test/java/com/example/libraryManagementSystem/LibraryManagementSystem/TestContainerConfiguration.java
@@ -0,0 +1,20 @@
+package com.example.libraryManagementSystem.LibraryManagementSystem;
+
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration
+public class TestContainerConfiguration {
+
+    @Bean
+    @ServiceConnection
+    PostgreSQLContainer<?> postgreSQLContainer() {
+        return new PostgreSQLContainer<>(
+                DockerImageName.parse("postgres:latest")
+        );
+    }
+}
