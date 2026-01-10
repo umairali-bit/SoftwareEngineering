5cb9549669d8be20b738b71e2a1a5885e83e8aa9
Umair Ali <umairmamoor@gmail.com>
2026-01-10 15:29:36 -0500

    new library-management-system

--- Backend/SpringBootTesting/MODULE7/project/library-management-system/src/test/java/com/umair/librarymanagement/LibraryManagementSystemApplicationTests.java
+++ Backend/SpringBootTesting/MODULE7/project/library-management-system/src/test/java/com/umair/librarymanagement/LibraryManagementSystemApplicationTests.java
@@ -1,15 +1,13 @@
-package com.example.libraryManagementSystem.LibraryManagementSystem;
+package com.umair.librarymanagement;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
 
 @SpringBootTest
 class LibraryManagementSystemApplicationTests {
 
 	@Test
 	void contextLoads() {
-
 	}
 
 }
