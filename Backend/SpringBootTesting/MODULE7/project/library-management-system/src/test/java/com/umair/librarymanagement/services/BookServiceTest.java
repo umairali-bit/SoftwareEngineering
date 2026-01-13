7cc3f8ff16cbeb1636511c7188a11b42d61ec741
Umair Ali <umairmamoor@gmail.com>
2026-01-13 02:34:36 -0500

    testCreateBook()

--- Backend/SpringBootTesting/MODULE7/project/library-management-system/src/test/java/com/umair/librarymanagement/services/BookServiceTest.java
+++ Backend/SpringBootTesting/MODULE7/project/library-management-system/src/test/java/com/umair/librarymanagement/services/BookServiceTest.java
@@ -1,20 +1,99 @@
 package com.umair.librarymanagement.services;
 
+import com.umair.librarymanagement.dtos.AuthorSummaryDTO;
+import com.umair.librarymanagement.dtos.BookDTO;
+import com.umair.librarymanagement.dtos.BookSummaryDTO;
+import com.umair.librarymanagement.entities.AuthorEntity;
+import com.umair.librarymanagement.entities.BookEntity;
+import com.umair.librarymanagement.repositories.AuthorRepository;
+import com.umair.librarymanagement.repositories.BookRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.*;
 
 
 @ExtendWith(MockitoExtension.class)
 class BookServiceTest {
 
+    @Mock
+    private BookRepository bookRepository;
+    @Mock
+    private AuthorRepository authorRepository;
+
+    @InjectMocks
+    private BookService bookService;
+
+    private BookEntity book;
+    private AuthorEntity author;
+
+    private BookDTO bookDTO;
+    private BookSummaryDTO bookSummaryDto;
+
+    private AuthorSummaryDTO authorSummaryDto;
+
     @BeforeEach
     void setUp() {
+
+        LocalDate date = LocalDate.of(2020, 1, 1);
+
+//      Book Entity
+        book = new BookEntity();
+        book.setTitle("Breaking Bad");
+        book.setPublishedDate(date);
+
+        //      Author Entity
+        author = new AuthorEntity();
+        author.setId(1L);
+        author.setName("Jessie Pinkman");
+
+
+        authorSummaryDto = new AuthorSummaryDTO();
+        authorSummaryDto.setId(author.getId());
+        authorSummaryDto.setName(author.getName());
+
+
+
+        bookDTO = new BookDTO();
+        bookDTO.setAuthor(authorSummaryDto);
+        bookDTO.setTitle("Breaking Bad");
+        bookDTO.setPublishedDate(date);
+
+
+
     }
 
     @Test
-    void createBook() {
+    void testCreateBook() {
+
+        when(authorRepository.findById(1L)).thenReturn(Optional.of(author));
+
+//      returns the exact object that your service passed into save() in this call.
+        when(bookRepository.save(any(BookEntity.class)))
+                .thenAnswer(inv -> inv.getArgument(0));
+
+        BookDTO result = bookService.createBook(bookDTO);
+
+        // Assert
+        assertThat(result).isNotNull();
+        assertThat(result.getTitle()).isEqualTo("Breaking Bad");
+        assertThat(result.getPublishedDate()).isEqualTo(bookDTO.getPublishedDate());
+        assertThat(result.getAuthor().getId()).isEqualTo(author.getId());
+
+        verify(authorRepository).findById(author.getId());
+        verify(bookRepository).save(any(BookEntity.class));
+
+
+
     }
 
     @Test
