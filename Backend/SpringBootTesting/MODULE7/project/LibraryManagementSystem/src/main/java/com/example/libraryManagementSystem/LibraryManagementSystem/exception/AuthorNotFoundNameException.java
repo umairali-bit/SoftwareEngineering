0f8ff335f2f7b748fd064933d12ebfe564776a11
Umair Ali <umairmamoor@gmail.com>
2026-01-14 06:55:24 -0500

    Remove unused target directory

--- Backend/SpringBootTesting/MODULE7/project/LibraryManagementSystem/src/main/java/com/example/libraryManagementSystem/LibraryManagementSystem/exception/AuthorNotFoundNameException.java
+++
@@ -1,7 +0,0 @@
-package com.example.libraryManagementSystem.LibraryManagementSystem.exception;
-
-public class AuthorNotFoundNameException extends  RuntimeException {
-    public AuthorNotFoundNameException(String name) {
-        super("Author not found with the name: " + name);
-    }
-}
--- Backend/SpringBootTesting/MODULE7/project/LibraryManagementSystem/src/main/java/com/example/libraryManagementSystem/LibraryManagementSystem/exception/BookNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.libraryManagementSystem.LibraryManagementSystem.exception;
-
-public class BookNotFoundException extends RuntimeException {
-    public BookNotFoundException(Long id) {
-        super("Book not found with ID: " + id);
-    }
-}
