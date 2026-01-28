8f2edee3654339efcbe49d779633e62fd1b1ced3
Umair Ali <umairmamoor@gmail.com>
2026-01-28 01:56:59 -0500

    spring Profiles

--- Backend/SpringBootDeploymentCICD/MODULE8/8.1/SetUpProductionRDSinAWS/testing-app/src/main/java/com/example/testing/testingapp/entities/Employee.java
+++ Backend/SpringBootDeploymentCICD/MODULE8/8.1/SetUpProductionRDSinAWS/testing-app/src/main/java/com/example/testing/testingapp/entities/Employee.java
@@ -6,9 +6,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.modelmapper.internal.bytebuddy.dynamic.loading.InjectionClassLoader;
 
-import javax.annotation.processing.Generated;
 
 @Entity
 @Data
@@ -20,9 +18,6 @@ public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-
-
-
     private String name;
 
     @Column(unique = true)
