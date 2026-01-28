9d47d818225e592ec293803ec4835852f4557a7a
Umair Ali <umairmamoor@gmail.com>
2026-01-28 01:50:28 -0500

    Spring profiles, production and development enviroment in spring boot

---
+++ Backend/SpringBootDeploymentCICD/MODULE8/8.2/SpringProdAndDevProfiles/testing-app/src/main/java/com/example/testing/testingapp/entities/Employee.java
@@ -0,0 +1,28 @@
+package com.example.testing.testingapp.entities;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @Column(unique = true)
+    private String email;
+
+    private Long salary;
+
+}
