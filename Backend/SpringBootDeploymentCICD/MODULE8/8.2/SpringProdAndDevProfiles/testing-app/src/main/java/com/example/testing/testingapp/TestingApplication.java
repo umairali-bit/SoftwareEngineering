9d94eecef2d766e516de51c5e37a7eb038702439
Umair Ali <umairmamoor@gmail.com>
2026-01-29 03:23:49 -0500

    troubleshooting

--- Backend/SpringBootDeploymentCICD/MODULE8/8.2/SpringProdAndDevProfiles/testing-app/src/main/java/com/example/testing/testingapp/TestingApplication.java
+++ Backend/SpringBootDeploymentCICD/MODULE8/8.2/SpringProdAndDevProfiles/testing-app/src/main/java/com/example/testing/testingapp/TestingApplication.java
@@ -2,6 +2,7 @@ package com.example.testing.testingapp;
 
 import com.example.testing.testingapp.services.DataService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,7 +11,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TestingApplication implements CommandLineRunner {
 
-    private final DataService dataService;
+//    private final DataService dataService;
+
+    @Value("${my.variable}")
+    private String myVariable;
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestingApplication.class, args);
@@ -18,7 +22,11 @@ public class TestingApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("The Data is: " + dataService.getData());
+
+        System.out.println("my variable: " + myVariable);
+
+
+//        System.out.println("The Data is: " + dataService.getData());
 
     }
 }
