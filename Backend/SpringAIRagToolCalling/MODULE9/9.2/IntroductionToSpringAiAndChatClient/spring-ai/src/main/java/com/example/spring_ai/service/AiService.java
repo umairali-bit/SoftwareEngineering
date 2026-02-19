0139e4ef0d226c33bd679df7463af285a21dc60d
Umair Ali <umairmamoor@gmail.com>
2026-02-19 03:23:16 -0500

    AI service, chat client

---
+++ Backend/SpringAIRagToolCalling/MODULE9/9.2/IntroductionToSpringAiAndChatClient/spring-ai/src/main/java/com/example/spring_ai/service/AiService.java
@@ -0,0 +1,21 @@
+package com.example.spring_ai.service;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AiService {
+
+    private ChatClient chatClient;
+
+    private String getJoke(String topic) {
+       return chatClient.prompt()
+                .user("Give me a joke on the topic: " +topic)
+                .call()
+                .content();
+    }
+
+}
