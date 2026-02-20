a7eb135f6f9ddc471ac9e1147f1ea71015beb739
Umair Ali <umairmamoor@gmail.com>
2026-02-20 04:14:32 -0500

    renderedText

--- Backend/SpringAIRagToolCalling/MODULE9/9.2/IntroductionToSpringAiAndChatClient/spring-ai/src/main/java/com/example/spring_ai/service/AiService.java
+++ Backend/SpringAIRagToolCalling/MODULE9/9.2/IntroductionToSpringAiAndChatClient/spring-ai/src/main/java/com/example/spring_ai/service/AiService.java
@@ -3,7 +3,10 @@ package com.example.spring_ai.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -11,10 +14,23 @@ public class AiService {
 
     private final ChatClient chatClient;
 
+
+
    public String getJoke(String topic) {
+
+       String systemPrompt = """
+            You are a sarcastic joker, you make poetic jokes in 4 lines.
+            You dont make jokes about politics.
+            Give me a joke on the topic: {topic}
+            """;
+
+       PromptTemplate promptTemplate = new PromptTemplate(systemPrompt);
+
+//       replace placeholders inside the prompt template with actual values
+       String renderedText = promptTemplate.render(Map.of("topic", topic));
+
        return chatClient.prompt()
-               .system("You are a sarcastic joker, give response in 1 line only.")
-                .user("Give me a joke on the topic: " +topic)
+                .user(renderedText)
                 .call()
                 .content();
     }
