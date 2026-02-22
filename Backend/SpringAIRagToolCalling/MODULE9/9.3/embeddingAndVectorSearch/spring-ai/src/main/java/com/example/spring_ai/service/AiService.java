f86a0cac587b45f52baa9fab04532e42a566e3b1
Umair Ali <umairmamoor@gmail.com>
2026-02-22 05:45:53 -0500

    EmbeddingModel

--- Backend/SpringAIRagToolCalling/MODULE9/9.3/embeddingAndVectorSearch/spring-ai/src/main/java/com/example/spring_ai/service/AiService.java
+++ Backend/SpringAIRagToolCalling/MODULE9/9.3/embeddingAndVectorSearch/spring-ai/src/main/java/com/example/spring_ai/service/AiService.java
@@ -6,6 +6,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.embedding.Embedding;
+import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.stereotype.Service;
 
 import java.util.Map;
@@ -15,6 +17,15 @@ import java.util.Map;
 public class AiService {
 
     private final ChatClient chatClient;
+
+    private final EmbeddingModel embeddingModel;
+
+    public float[] getEmbedding(String text) {
+        return embeddingModel.embed(text);
+
+    }
+
+
 
 
     public String getJoke(String topic) {
