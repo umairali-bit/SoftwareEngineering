fab150cfd76bf7839e9f17ba39415a11dc3ffb62
Umair Ali <umairmamoor@gmail.com>
2026-02-28 08:23:33 -0500

    setting Simple Logger Advisor as the default Advisor

--- Backend/SpringAIRagToolCalling/MODULE9/9.5/MasteringAdvisors/spring-ai/src/main/java/com/example/spring_ai/config/AiConfig.java
+++ Backend/SpringAIRagToolCalling/MODULE9/9.5/MasteringAdvisors/spring-ai/src/main/java/com/example/spring_ai/config/AiConfig.java
@@ -2,6 +2,7 @@ package com.example.spring_ai.config;
 
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
 import org.springframework.beans.factory.BeanRegistrarDslMarker;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,7 @@ public class AiConfig {
     @Bean
     public ChatClient  chatClient(ChatClient.Builder builder){
         return builder
+                .defaultAdvisors(new SimpleLoggerAdvisor())
                 .build();
     }
 
