ef166eaa6dad67f95f5ae02fca2fb0a916c91188
Umair Ali <umairmamoor@gmail.com>
2026-03-05 03:29:45 -0500

    weather tool - testing postman

--- Backend/SpringAIRagToolCalling/MODULE9/9.6/toolCallingInSpringAI/spring-ai/src/main/java/com/example/spring_ai/controller/ChatController.java
+++ Backend/SpringAIRagToolCalling/MODULE9/9.6/toolCallingInSpringAI/spring-ai/src/main/java/com/example/spring_ai/controller/ChatController.java
@@ -4,6 +4,7 @@ import com.example.spring_ai.tool.TravellingTools;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ public class ChatController {
     private final TravellingTools travellingTools;
 
     @PostMapping("/chat")
-    public String chat(@RequestParam String message) {
+    public String chat(@RequestBody String message) {
         return chatClient.prompt()
                 .user(message)
                 .tools(travellingTools)
