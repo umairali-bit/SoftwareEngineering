2ef53b6b806b6de4b468992440da54b3d32d0f6e
Umair Ali <umairmamoor@gmail.com>
2026-03-10 11:17:27 -0400

    FlightBooking - createBooking tool

--- Backend/SpringAIRagToolCalling/MODULE9/9.6/toolCallingInSpringAI/spring-ai/src/main/java/com/example/spring_ai/tool/FlightBookingTools.java
+++ Backend/SpringAIRagToolCalling/MODULE9/9.6/toolCallingInSpringAI/spring-ai/src/main/java/com/example/spring_ai/tool/FlightBookingTools.java
@@ -1,4 +1,44 @@
 package com.example.spring_ai.tool;
 
+import com.example.spring_ai.entity.FlightBooking;
+import com.example.spring_ai.repository.FlightBookingRepository;
+import com.example.spring_ai.service.FlightBookingServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.tool.annotation.Tool;
+import org.springframework.ai.tool.annotation.ToolParam;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+
+@Component
+@RequiredArgsConstructor
 public class FlightBookingTools {
+    private final FlightBookingServiceImpl flightBookingService;
+
+    @Tool(
+            name = "flight_booking_tool",
+            description = "Create a new flight booking for a user"
+
+    )
+    public FlightBooking createFlightBooking(
+            @ToolParam(description = "Unique user id")
+            String userId,
+            @ToolParam(description = "destination")
+            String destination,
+            @ToolParam(description = "departure time in ISO-8601 format")
+            Instant departureTime) {
+
+        var flightBooking = flightBookingService.createFlightBooking(userId, destination, departureTime);
+
+        return new BookingReponse(
+                flightBooking.getId(),
+                flightBooking.getDestination(),
+                flightBooking.getDepartureTime(),
+                flightBooking.getBookingStatus()
+        );
+
+
+
+
+    }
 }
