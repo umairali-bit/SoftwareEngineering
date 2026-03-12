849f9699c22875958ec2e04e4200e7c0bc873cb4
Umair Ali <umairmamoor@gmail.com>
2026-03-12 02:24:26 -0400

    The Scarcastic Poet

---
+++ Backend/SpringAIRagToolCalling/MODULE9/sarcastic-poet/src/main/java/com/example/sarcasticpoet/SarcasticPoetApplication.java
@@ -0,0 +1,13 @@
+package com.example.sarcasticpoet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SarcasticPoetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SarcasticPoetApplication.class, args);
+	}
+
+}
