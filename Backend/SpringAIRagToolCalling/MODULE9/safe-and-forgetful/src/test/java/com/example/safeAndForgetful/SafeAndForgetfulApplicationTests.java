2727235a7a115cacaaf99f4cd95071d3bd746adc
Umair Ali <umairmamoor@gmail.com>
2026-03-23 07:23:07 -0400

    safe and forgetful

---
+++ Backend/SpringAIRagToolCalling/MODULE9/safe-and-forgetful/src/test/java/com/example/safeAndForgetful/SafeAndForgetfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.safeAndForgetful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafeAndForgetfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
