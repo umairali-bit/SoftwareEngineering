a8be08883687e89b794e2dcf0ad72a54c59b1bdf
Umair Ali <umairmamoor@gmail.com>
2026-05-29 08:42:47 +0800

    simple AI configuration

---
+++ Backend/SpringAIRagAndToolCalling/Module9/9.2/introductionToSpringAIandChatClient/spring-ai/src/main/java/com/example/spring_ai/config/AIConfig.java
@@ -0,0 +1,17 @@
+package com.example.spring_ai.config;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AIConfig {
+
+    @Bean
+    public ChatClient chatClient(ChatClient.Builder chatClientBuilder) {
+        return  chatClientBuilder
+
+
+                .build();
+    }
+}
