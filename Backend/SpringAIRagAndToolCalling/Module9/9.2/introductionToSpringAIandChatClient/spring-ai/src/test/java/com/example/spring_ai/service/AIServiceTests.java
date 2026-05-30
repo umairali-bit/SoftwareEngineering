1f6394c7bded2a9ef896388f95e61645b2dad258
Umair Ali <umairmamoor@gmail.com>
2026-05-30 08:35:24 +0800

    Testing AI service.

---
+++ Backend/SpringAIRagAndToolCalling/Module9/9.2/introductionToSpringAIandChatClient/spring-ai/src/test/java/com/example/spring_ai/service/AIServiceTests.java
@@ -0,0 +1,16 @@
+package com.example.spring_ai.service;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AIServiceTests {
+
+    @Autowired
+    private AIService aiService;
+
+    @Test
+    public void testGetJoke() {}
+}
