519db625b66a4123d9faee41b9fa994910c27803
Umair Ali <umairmamoor@gmail.com>
2026-06-01 13:18:39 +0800

    Adding system prompt by using PromptTemplate

--- Backend/SpringAIRagAndToolCalling/Module9/9.2/introductionToSpringAIandChatClient/spring-ai/src/main/java/com/example/spring_ai/service/AIService.java
+++ Backend/SpringAIRagAndToolCalling/Module9/9.2/introductionToSpringAIandChatClient/spring-ai/src/main/java/com/example/spring_ai/service/AIService.java
@@ -2,7 +2,10 @@ package com.example.spring_ai.service;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.prompt.PromptTemplate;
 import org.springframework.stereotype.Service;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -11,11 +14,28 @@ public class AIService {
     private final ChatClient chatClient;
 
     public String getJoke(String topic) {
+//    system prompt
+    String systemPrompt = """
+        You are a sarcastic joker. Give response in 4 lines.
+        Dont mention politics at all.
+        Get me a joke on the topic: {topic}
+        """;
+
+    PromptTemplate promptTemplate = new PromptTemplate(systemPrompt);
+    String renderText = promptTemplate.render(Map.of("topic", topic));
+
         return chatClient.prompt()
-                .system("You are a sarcastic joker. Give response in 1 line")
-                .user("Give me a joke on topic: " + topic)
+                .user(renderText)
                 .call()
                 .content();
+
+
+//    public String getJoke(String topic) {
+//        return chatClient.prompt()
+//                .system("You are a sarcastic joker. Give response in 1 line")
+//                .user("Give me a joke on topic: " + topic)
+//                .call()
+//                .content();
     }
 
 }
