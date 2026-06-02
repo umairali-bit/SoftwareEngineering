505242d6bdf00cd2c4774a8820d277d73f3e9a89
Umair Ali <umairmamoor@gmail.com>
2026-06-02 16:05:19 +0800

    Embedding and Vector Search

---
+++ Backend/SpringAIRagAndToolCalling/Module9/9.3/embeddingAndVectorSearch/spring-ai/src/main/java/com/example/spring_ai/service/AIService.java
@@ -0,0 +1,54 @@
+package com.example.spring_ai.service;
+
+import com.example.spring_ai.dto.Joke;
+import lombok.RequiredArgsConstructor;
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class AIService {
+
+    private final ChatClient chatClient;
+
+
+    public String getJoke(String topic) {
+//    system prompt
+    String systemPrompt = """
+        You are a sarcastic joker. Give response in exactly 4 lines.
+        Dont mention politics at all.
+        Get me a joke on the topic: {topic}
+        """;
+
+    PromptTemplate promptTemplate = new PromptTemplate(systemPrompt);
+    String renderText = promptTemplate.render(Map.of("topic", topic));
+
+        var response = chatClient.prompt()
+                .user(renderText)
+                .advisors(
+                        new SimpleLoggerAdvisor()
+                )
+                .call()
+//                .content();
+//                .chatClientResponse();
+                .entity(Joke.class);
+
+//        return response.chatResponse().getResult().getOutput().getText();
+        return response.text();
+
+//    public String getJoke(String topic) {
+//        return chatClient.prompt()
+//                .system("You are a sarcastic joker. Give response in 1 line")
+//                .user("Give me a joke on topic: " + topic)
+//                .call()
+//                .content();
+    }
+
+}
+
+
---
+++ Backend/SpringAIRagAndToolCalling/Module9/9.3/embeddingAndVectorSearch/spring-ai/src/test/java/com/example/spring_ai/service/AIServiceTests.java
@@ -0,0 +1,20 @@
+package com.example.spring_ai.service;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class AIServiceTests {
+
+    @Autowired
+    private AIService aiService;
+
+    @Test
+    public void testGetJoke() {
+
+        var joke = aiService.getJoke("Dogs");
+        System.out.println(joke);
+    }
+}
