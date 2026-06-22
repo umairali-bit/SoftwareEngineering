3d5700ae06ed878f5563d7ee050dfd62d56c4d96
Umair Ali <umairmamoor@gmail.com>
2026-06-22 12:26:11 +0800

    Spring Boot default caching

---
+++ Backend/CachingAndConcurrentTransactionManagement/Module11/11.2/SpringBootDefaultCaching/cachingApp/src/main/java/com/example/cachingApp/CachingAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.cachingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CachingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CachingAppApplication.class, args);
+	}
+
+}
