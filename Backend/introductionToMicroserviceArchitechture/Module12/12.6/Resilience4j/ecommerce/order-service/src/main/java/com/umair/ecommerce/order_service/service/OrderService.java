533f93c449d568716ca8bff433a77148ed9d575d
Umair Ali <umairmamoor@gmail.com>
2026-09-23 10:22:29 +0800

    feat: add Resilience4j rate limiter logic for service communication

--- Backend/introductionToMicroserviceArchitechture/Module12/12.6/Resilience4j/ecommerce/order-service/src/main/java/com/umair/ecommerce/order_service/service/OrderService.java
+++ Backend/introductionToMicroserviceArchitechture/Module12/12.6/Resilience4j/ecommerce/order-service/src/main/java/com/umair/ecommerce/order_service/service/OrderService.java
@@ -6,6 +6,7 @@ import com.umair.ecommerce.order_service.dto.OrderRequestDto;
 import com.umair.ecommerce.order_service.entity.Order;
 import com.umair.ecommerce.order_service.entity.OrderItem;
 import com.umair.ecommerce.order_service.entity.enums.OrderStatus;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -42,6 +43,7 @@ public class OrderService {
     }
 
     @Retry(name = "inventoryRetry", fallbackMethod = "createOrderFallback")
+    @RateLimiter(name = "inventoryRateLimiter", fallbackMethod = "createOrderFallback")
     public OrderRequestDto createOrders(OrderRequestDto orderRequestDto) {
 
         log.info("creating orders ({})", orderRequestDto);
