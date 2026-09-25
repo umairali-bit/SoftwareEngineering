ec501c5343fed2d2660b02f2162c4913a5504f89
Umair Ali <umairmamoor@gmail.com>
2026-09-25 12:08:51 +0800

    starting front-end from scratch

---
+++ Frontend/introductionToJavaScript/script.js
@@ -0,0 +1,19 @@
+
+// const x = "umair"
+// console.log(x);
+
+// for (let i = 0; i < 10; i++) {
+//     console.log(i);
+// }
+
+const obj = {
+    name: "Umair",
+    age: 20,
+}
+console.log(obj.name);
+console.log(obj.age);
+
+const x = 10;
+const y = String(x);
+console.log(typeof y);
+
